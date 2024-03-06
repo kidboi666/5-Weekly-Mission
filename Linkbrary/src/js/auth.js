afdf671ef769881d76bc899e96f4931465135a92
bk-git-hub <bkprivate99@gmail.com>
2024-03-06 17:55:45 +0900

    feat: Add email validity check feature

--- Linkbrary/src/js/auth.js
+++ Linkbrary/src/js/auth.js
@@ -4,17 +4,32 @@ const passwordInput = document.querySelector("#password");
 function checkEmail() {
   const parent = emailInput.parentElement;
   const inputValue = emailInput.value;
-  if (inputValue === "") {
-    let newP = document.createElement("P");
-    newP.textContent = "이메일을 입력해주세요";
-    newP.className = "error-message";
-    parent.appendChild(newP);
+  const isEmpty =
+    inputValue === ""
+      ? generateErrorMessage(parent, "이메일을 입력해주세요")
+      : false;
+
+  if (!isEmpty && !isValidEmail(inputValue)) {
+    generateErrorMessage(parent, "올바른 이메일 주소가 아닙니다.");
   }
 }
 
 function handleFocusIn(event) {
   const target = event.target;
   removeErrorMessage(target);
+}
+
+function isValidEmail(inputValue) {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(inputValue);
+}
+
+function generateErrorMessage(parent, errorText) {
+  let newP = document.createElement("P");
+  newP.textContent = errorText;
+  newP.className = "error-message";
+  parent.appendChild(newP);
+  return true;
 }
 
 function removeErrorMessage(element) {
