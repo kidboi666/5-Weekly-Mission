6a7415f0e879e099b262d6da0a4bfadc40a965af
DESKTOP-80GNNG0\WORK <ssjeond@naver.com>
2024-03-07 09:01:05 +0900

    fix: Removed submit function in Signup.js

--- 4th_week_mission/signup.js
+++ 4th_week_mission/signup.js
@@ -44,31 +44,7 @@ document.getElementById('passwordRepeat').addEventListener('focusout', function(
   }  
 })
 
-// Submit
-document.querySelector('.cta').addEventListener('click', submitSign);
 
-document.addEventListener('keydown', function submitByEnter(e) {
-  if (e.key === 'Enter') {
-      submitSign();
-  }
-})
-
-function submitSign() {
-  const email = document.getElementById('email').value.trim();
-  const password = document.getElementById('password').value.trim();
-
-    // 입력된 이메일과 비밀번호가, 요구된 사항과 같은지 검사
-  if (email === "test@codeit.com" && password === "codeit101") {
-    location.href = "/folder.html"; // 로그인 버튼 타입을 submit으로 하니 작동이 되지 않아, button type으로 설정함
-  }else {
-    // 입력된 이메일과 비밀번호가 다를 시에, 오류 메세지 출력
-    const emailError = document.getElementById('email_error');
-    const passwordError = document.getElementById('pwd_error');
-
-    emailError.textContent = "이메일을 확인해 주세요."
-    passwordError.textContent = "비밀번호를 확인해 주세요."
-  }
-}
 
 // eye-button 클릭에 따라 eye-button on/off를 토글하고, password input type을 변경함 
 const showPassword = document.querySelector("#showPassword");
