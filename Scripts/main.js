2a67d24e01e2caf2283a45e90d74a29beba3ebc3
은지 <103625348+eunji-0623@users.noreply.github.com>
2024-03-08 15:44:06 +0900

    비밀번호 입력 시에도 경고메세지가 적용되도록 main.js 파일 수정
    1. RemoveEmailWarning -> RemoveWarningMsg 로 변경하여 비밀번호에도
       적용되도록 변경
    2. FormFocusOut에 비밀번호도 체크할 수 있도록 변경

--- Scripts/main.js
+++ Scripts/main.js
@@ -2,6 +2,7 @@ import * as enums from "./enums.js";
 
 const form = document.querySelector("form");
 const email = document.querySelector("#email");
+const pw = document.querySelector("#pw");
 const emailFormat = /^([0-9a-zA-Z_\.-]+)@([0-9a-zA-Z_-]+)(\.[0-9a-zA-Z_-]+){1,2}$/;
 
 // 경고 메세지 추가
@@ -13,27 +14,33 @@ function MakeWarningMsg(_text, pos) {
 }
 
 // 이메일 부분의 경고 메세지 제거
-function RemoveEmailWarning() {
-  if (email.nextElementSibling.classList.contains("warningMsg")) {
-    email.nextElementSibling.remove();
+function RemoveWarningMsg(e) {
+  if (e.target.nextElementSibling.classList.contains("warningMsg")) {
+    e.target.nextElementSibling.remove();
   }
-  if (email.classList.contains("warningForm")) {
-    email.classList.remove("warningForm");
+  if (e.target.classList.contains("warningForm")) {
+    e.target.classList.remove("warningForm");
   }
 }
 
 function FormFocusOut(e) {
   // 입력 값이 없을 때
   if (e.target.value === "") {
-    MakeWarningMsg(enums.WargingMsg.EMAIL_NULL, email);
-    email.classList.add("warningForm");
+    if (e.target.name === "email") {
+      MakeWarningMsg(enums.WargingMsg.EMAIL_NULL, e.target);
+    } else {
+      MakeWarningMsg(enums.WargingMsg.PW_NULL, e.target);
+    }
+    e.target.classList.add("warningForm");
   }
   // 올바른 이메일 형식이 아닐 때
-  else if (emailFormat.test(e.target.value) === false) {
-    MakeWarningMsg(enums.WargingMsg.EMAIL_INVALID, email);
-    email.classList.add("warningForm");
+  else if (emailFormat.test(e.target.value) === false && e.target.name === "email") {
+    MakeWarningMsg(enums.WargingMsg.EMAIL_INVALID, e.target);
+    e.target.classList.add("warningForm");
   }
 }
 
 email.addEventListener("focusout", FormFocusOut);
-email.addEventListener("focusin", RemoveEmailWarning);
+email.addEventListener("focusin", RemoveWarningMsg);
+pw.addEventListener("focusout", FormFocusOut);
+pw.addEventListener("focusin", RemoveWarningMsg);
