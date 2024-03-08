2e8f2d6b1ca5c261c6097b2c839c964069dc5ad9
GoldenHamsterK <kimdajin0928@gmail.com>
2024-03-08 18:45:00 +0900

    feat: 로그인 인풋 error 추가

---
+++ signin.js
@@ -0,0 +1,57 @@
+const singinForm = document.querySelector(`.singin-form`);
+const inputEmail = document.querySelector(`.email-input-group`);
+const inputPassword = document.querySelector(`.password-input-group`);
+const singinBtn = document.querySelector(`#singin-btn`);
+const emailErrorText = document.querySelector('.email-input-message');
+const emailInput = document.querySelector('.email-input');
+const passwordErrorText = document.querySelector('.password-input-message');
+const passwordInput = document.querySelector('.password-input');
+
+inputEmail.addEventListener(`focusout`,(e) => {
+  emailText = e.target.value;
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (emailRegex.test(emailText) === false){
+    if(emailText === ''){
+      emailErrorText.textContent = '이메일을 입력해주세요.';
+      emailInput.classList.add('error-input');
+    }
+    else{
+      emailErrorText.textContent = '올바른 이메일 형식이 아닙니다.';
+      emailInput.classList.add('error-input');
+    }
+  }
+  else{
+    emailErrorText.textContent = '';
+    emailInput.classList.remove('error-input');
+  }
+});
+
+inputPassword.addEventListener(`focusout`,(e) => {
+  passwordText = e.target.value;
+  if(passwordText === ''){
+    passwordErrorText.textContent = '비밀번호를 입력해주세요.';
+    passwordInput.classList.add('error-input');
+  }
+  else{
+    passwordErrorText.textContent = '';
+    passwordInput.classList.remove('error-input');
+  }
+});
+
+singinBtn.addEventListener('click',(e) => {
+  if(emailInput.value === `test@codeit.com` &&
+    passwordInput.value === `codeit101`){
+    location.href = "/folder.html";
+    }
+  else{
+    emailErrorText.textContent = '이메일을 확인해주세요.';
+    emailInput.classList.add('error-input');
+    passwordErrorText.textContent = '비밀번호를 확인해주세요.';
+    passwordInput.classList.add('error-input');
+    e.preventDefault();
+  }
+});
+
+
+
+
