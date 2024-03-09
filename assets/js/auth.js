3e4fc801022c56c7902acfce8ec84e35fe999b2b
daeun <qzsz797979@gmail.com>
2024-03-09 19:40:28 +0900

    로그인 시도를 하면 이메일 input 아래에 “이메일을 확인해 주세요.”, 비밀번호 input 아래에 “비밀번호를 확인해 주세요.” 에러 메세지를 보임

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -3,12 +3,16 @@ const email = document.querySelector("#email");
 const password = document.querySelector("#password");
 
 function showMessageToWriteInput(e) {
-  const checkMessage = e.target.parentElement.parentElement.childNodes.length !== 5;
+  const checkMessage =
+    e.target.parentElement.parentElement.childNodes.length !== 5;
   if (!e.target.value) {
     if (!checkMessage) {
       const notice = document.createElement("div");
-      const textContent = e.target.id === 'email' ? "이메일을 입력해 주세요." : "비밀번호를 입력해 주세요.";
-      notice.classList.add("auth-form__notice-enter-input");
+      const textContent =
+        e.target.id === "email"
+          ? "이메일을 입력해 주세요."
+          : "비밀번호를 입력해 주세요.";
+      notice.classList.add("auth-form__notice");
       notice.textContent = textContent;
       e.target.parentElement.after(notice);
       e.target.classList.add("input-border--red");
@@ -33,7 +37,7 @@ function showMessageToWriteInEmailFormat(e) {
       e.target.parentElement.nextElementSibling.textContent = textContent;
     } else {
       const notice = document.createElement("div");
-      notice.classList.add("auth-form__notice-enter-input");
+      notice.classList.add("auth-form__notice");
       notice.textContent = textContent;
       e.target.parentElement.after(notice);
       e.target.classList.add("input-border--red");
@@ -46,15 +50,34 @@ function verifyInvalidEmail(e) {
   e.target.value && showMessageToWriteInEmailFormat(e);
 }
 
-function LinkIfAdmin(e) {
+function login(e) {
   e.preventDefault();
   const adminEmail = "test@codeit.com";
   const adminPassword = "codeit101";
-  if (email.value === adminEmail && password.value === adminPassword) {
+  const isAdmin =
+    email.value === adminEmail && password.value === adminPassword;
+  if (isAdmin) {
     location.href = "/folder";
+  } else {
+    if (!email.parentElement.nextElementSibling) {
+      const noticeEmail = document.createElement("div");
+      const emailTextContent = "이메일을 확인해 주세요.";
+      noticeEmail.classList.add("auth-form__notice");
+      noticeEmail.textContent = emailTextContent;
+      email.parentElement.after(noticeEmail);
+      email.classList.add("input-border--red");
+    }
+    if (!password.parentElement.nextElementSibling) {
+      const noticePassword = document.createElement("div");
+      const passwordTextContent = "비밀번호를 확인해 주세요.";
+      noticePassword.classList.add("auth-form__notice");
+      noticePassword.textContent = passwordTextContent;
+      password.parentElement.after(noticePassword);
+      password.classList.add("input-border--red");
+    }
   }
 }
 
 email.addEventListener("focusout", verifyInvalidEmail);
 password.addEventListener("focusout", showMessageToWriteInput);
-form.addEventListener("submit", LinkIfAdmin);
+form && form.addEventListener("submit", login);
