1d0cb628d6db7a20ed612644f424ced8f368c56d
juan0444 <dhwjs5267@gmail.com>
2024-03-10 05:13:39 +0900

    주석 삭제

--- assets/js/eye.js
+++ assets/js/eye.js
@@ -1,20 +1,15 @@
-// 비밀번호의 문자열이 가려질 때는 눈 모양 아이콘에는 사선이 그어져있고, 
-// 비밀번호의 문자열이 보일 때는 사선이 없는 눈 모양 아이콘이 보이도록 합니다.
+import { inputPassword } from '/assets/js/login.js';
+const eyeBtn = document.querySelector('.sign-pw__eye-btn');
+const eyeImg = document.querySelector('.eye-change');
 
-const eyebtn = document.querySelector('.sign__pw-eye-off');
-const eyeimg = document.querySelector('.eye-offtest');
-
-//import로 가져오는 걸로 수정
-const TESTPW = document.querySelector('#PW');
-
-function eyechange () {
-  eyeimg.classList.toggle('addeye');
+function eyeChange () {
+  eyeImg.classList.toggle('eye-on');
   
-  if (TESTPW.type === "password") {
-    TESTPW.type = "text";
+  if (inputPassword.type === "password") {
+    inputPassword.type = "text";
   } else {
-    TESTPW.type = "password";
+    inputPassword.type = "password";
   }
 }
 
-eyebtn.addEventListener('click', eyechange);
+eyeBtn.addEventListener('click', eyeChange);
--- assets/js/login.js
+++ assets/js/login.js
@@ -1,134 +1,90 @@
-// inpuut 가져오기 
-const TESTEMAIL = document.querySelector('#EMAIL');
-const TESTPW = document.querySelector('#PW');
+const inputEmail = document.querySelector('#input-email');
+export const inputPassword = document.querySelector('#input-pw');
+const loginButton = document.querySelector('.sign-btn');
+const emailErrorMessage = document.querySelector('.email-error-message');
+const pwErrorMessage = document.querySelector('.pw-error-message');
 
-// 버튼
-const btn = document.querySelector('.sign-btn');
+const emailAddMessage = document.createElement('div');
+emailAddMessage.classList.add('error_message');
+const pwAddMessage = document.createElement('div');
+pwAddMessage.classList.add('error_message');
+let messageText = document.createTextNode('');
 
-// 에러메시지
-const errorNOmessage = document.querySelector('.errortestNOemail'); //이메일
-const errorNOPW = document.querySelector('.errortestNOPW'); //비밀번호
-
-// 테스트 데이터
 const data = [{email: 'test@codeit.com', password: 'codeit101'}];
 
- // 에러 메시지 div 만들기
-const addtext = document.createElement('div');
-addtext.classList.add('error_message');
-
-const addtextPW = document.createElement('div');
-addtextPW.classList.add('error_message');
-
- // 에러 텍스트 추가
-let addmessage = document.createTextNode('');
-
-//-------------------------모듈------------------------
-
-// ----------------------------------------------------------------
-// 사용자 email 포커스 아웃
-function emailIogout() {  
-  const testemail = this.value;
-  console.log('email 포커스 아웃', testemail);
-  addtext.innerText = '';
+// email
+function emailCheck() {  
+  const emailValue = this.value;
+  emailAddMessage.innerText = '';
   
-  if (testemail.length > 0) {
+  if (emailValue.length > 0) {
     const emailTEST = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
 
-    if (!emailTEST.test(testemail)) {
-      // 2. 이메일 input에서 focus out 할 때, 이메일 형식에 맞지 않는 값이 있는 경우 아래에 “올바른 이메일 주소가 아닙니다.” 에러 메세지를 보입니다.
-      // @ 포함되지 않은 이메일 입력일 경우
-      console.log('다시 입력이 필요');
-      // 에러 텍스트 추가
-      addmessage = document.createTextNode('올바른 이메일 주소가 아닙니다.');
-      addtext.appendChild(addmessage);
+    if (!emailTEST.test(emailValue)) {
+      messageText = document.createTextNode('올바른 이메일 주소가 아닙니다.');
+      emailAddMessage.appendChild(messageText);
 
-      // 부모div에 추가
-      errorNOmessage.appendChild(addtext);
-      console.log('--------------------------');
-    } else {
-      // 적합한 이메일 형식
-      console.log('@ 포함되어 있음');
-      console.log('--------------------------');
+      emailErrorMessage.appendChild(emailAddMessage);
     }
   } else {
-    // 1. 이메일 input에서 focus out 할 때, 값이 없을 경우 아래에 “이메일을 입력해 주세요.” 에러 메세지를 보입니다.
-    console.log('다시 입력이 필요');
+    messageText = document.createTextNode('이메일을 입력해 주세요.');
+    emailAddMessage.appendChild(messageText);
 
-    // 에러 텍스트 추가
-    addmessage = document.createTextNode('이메일을 입력해 주세요.');
-    addtext.appendChild(addmessage);
-
-    // 부모div에 추가
-    errorNOmessage.appendChild(addtext);
-
-    console.log('--------------------------');
+    emailErrorMessage.appendChild(emailAddMessage);
   }
 }
 
-// ----------------------------------------------------------------
-// 4. 비밀번호 input에서 focus out 할 때, 값이 없을 경우 아래에 “비밀번호를 입력해 주세요.” 에러 메세지를 보입니다.
-// 사용자 pw 포커스 아웃
-function PWIogout() {
-  const testpw = this.value;
-  console.log('pw 포커스 아웃', testpw);
-  addtextPW.innerText = '';
+// password
+function pwCheck() {
+  const inputPassword = this.value;
+  console.log('pw 포커스 아웃', inputPassword);
+  pwAddMessage.innerText = '';
 
-  // 값이 없을 경우
-  if (testpw.length == 0) {
-    // 에러 텍스트 추가
-    addmessage = document.createTextNode('비밀번호를 입력해 주세요.');
-    addtextPW.appendChild(addmessage);
+  if (inputPassword.length == 0) {
+    messageText = document.createTextNode('비밀번호를 입력해 주세요.');
+    pwAddMessage.appendChild(messageText);
   
-    // 부모div에 추가
-    errorNOPW.appendChild(addtextPW);
+    pwErrorMessage.appendChild(pwAddMessage);
   }
 }
 
-
-// ----------------------------------------------------------------
-// 3. 이메일: test@codeit.com, 비밀번호: codeit101 으로 로그인 시도할 경우, “/folder” 페이지로 이동합니다.
-// 6. 로그인 버튼 클릭 또는 Enter키 입력으로 로그인 실행돼야 합니다.
-// 변수 이름 수정
+// 로그인
 function login() {
-  // 사용자 input을 변수에 담기
-  const userinputEmail = TESTEMAIL.value;
-  const userinputPW = TESTPW.value;
+  emailAddMessage.innerText = '';
+  pwAddMessage.innerText = '';
+
+  const userinputEmail = inputEmail.value;
+  const userinputPW = inputPassword.value;
 
   const user = data.find((d, i) => {
     console.log(i);
     return d.email === userinputEmail && d.password === userinputPW;
   });
 
-// 5. 이외의 로그인 시도의 경우, 이메일 input 아래에 “이메일을 확인해 주세요.”, 
-//    비밀번호 input 아래에 “비밀번호를 확인해 주세요.” 에러 메세지를 보입니다.
-
   const message = user 
     ? window.location.href = "/folder.html"
     : 
-      // 에러 텍스트 추가
-      addtext.innerText = '';
-      addmessage = document.createTextNode('이메일을 확인해 주세요.');
-      addtext.appendChild(addmessage);
-      // 에러 텍스트 추가
-      addtextPW.innerText = '';
-      addmessage = document.createTextNode('비밀번호를 확인해 주세요.');
-      addtextPW.appendChild(addmessage);
+      emailAddMessage.innerText = '';
+      messageText = document.createTextNode('이메일을 확인해 주세요.');
+      emailAddMessage.appendChild(messageText);
 
-      // 부모div에 추가
-      errorNOmessage.appendChild(addtext);
-      // 부모div에 추가
-      errorNOPW.appendChild(addtextPW);
+      pwAddMessage.innerText = '';
+      messageText = document.createTextNode('비밀번호를 확인해 주세요.');
+      pwAddMessage.appendChild(messageText);
+
+
+      emailErrorMessage.appendChild(emailAddMessage);
+      pwErrorMessage.appendChild(pwAddMessage);
     ;
 
   console.log(message);
 }
 
-// ----------------------------------------------------------------
 // 에러 메시지 출력
-TESTEMAIL.addEventListener('blur', emailIogout);
-TESTPW.addEventListener('blur', PWIogout);
+inputEmail.addEventListener('blur', emailCheck);
+inputPassword.addEventListener('blur', pwCheck);
 
 //로그인 버튼(enter) 클릭
-btn.addEventListener('click', login);
-TESTEMAIL.addEventListener('keypress', (e) => e.code === 'Enter' && login());
-TESTPW.addEventListener('keypress', (e) => e.code === 'Enter' && login());
+loginButton.addEventListener('click', login);
+inputEmail.addEventListener('keypress', (e) => e.code === 'Enter' && login());
+inputPassword.addEventListener('keypress', (e) => e.code === 'Enter' && login());
