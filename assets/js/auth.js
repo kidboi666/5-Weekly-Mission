6b16eb9e6a9419e09982744507c9874026f7067c
daeun <qzsz797979@gmail.com>
2024-03-09 17:55:30 +0900

    이메일: test@codeit.com, 비밀번호: codeit101 으로 로그인 시도할 경우, “/folder” 페이지로 이동

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -1,20 +1,21 @@
-const email = document.querySelector('#email');
-const password = document.querySelector('#password');
+const form = document.querySelector("#signinForm");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
 
 function showMessageToWriteInput(e) {
-  if(!e.target.value) {
-    if(!e.target.nextElementSibling) {
-      const notice = document.createElement('div');
-      notice.classList.add('auth-form__notice-enter-input');
-      notice.textContent = '이메일을 입력해 주세요.';
+  if (!e.target.value) {
+    if (!e.target.nextElementSibling) {
+      const notice = document.createElement("div");
+      notice.classList.add("auth-form__notice-enter-input");
+      notice.textContent = "이메일을 입력해 주세요.";
       e.target.after(notice);
-      e.target.classList.add('input-border--red');
+      e.target.classList.add("input-border--red");
     }
     e.target.focus();
   } else {
-    if(e.target.nextElementSibling) {
+    if (e.target.nextElementSibling) {
       e.target.nextElementSibling.remove();
-      e.target.classList.remove('input-border--red');
+      e.target.classList.remove("input-border--red");
     }
   }
 }
@@ -25,22 +26,33 @@ function checkEmailFormat(e) {
 }
 
 function showMessageToWriteInEmailFormat(e) {
-  if(!checkEmailFormat(e)) {
-    if(e.target.nextElementSibling) {
-      e.target.nextElementSibling.textContent = '올바른 이메일 주소가 아닙니다.';
+  if (!checkEmailFormat(e)) {
+    if (e.target.nextElementSibling) {
+      e.target.nextElementSibling.textContent =
+        "올바른 이메일 주소가 아닙니다.";
     } else {
-      const notice = document.createElement('div');
-      notice.classList.add('auth-form__notice-enter-input');
-      notice.textContent = '올바른 이메일 주소가 아닙니다.';
+      const notice = document.createElement("div");
+      notice.classList.add("auth-form__notice-enter-input");
+      notice.textContent = "올바른 이메일 주소가 아닙니다.";
       e.target.after(notice);
-      e.target.classList.add('input-border--red');
+      e.target.classList.add("input-border--red");
     }
   }
 }
 
-function verifyInvalidInput(e) {
+function LinkIfAdmin(e) {
+  e.preventDefault();
+  const adminEmail = "test@codeit.com";
+  const adminPassword = "codeit101";
+  if (email.value === adminEmail && password.value === adminPassword) {
+    location.href = "/folder";
+  }
+}
+
+function verifyInvalidEmail(e) {
   showMessageToWriteInput(e);
   e.target.value && showMessageToWriteInEmailFormat(e);
 }
 
-email.addEventListener('focusout', verifyInvalidInput);
+email.addEventListener("focusout", verifyInvalidEmail);
+form.addEventListener("submit", LinkIfAdmin);
