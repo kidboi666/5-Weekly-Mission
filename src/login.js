bc56af22c619f050bf680c211faa2b1d34556e02
ChoTD <jo7sy8@gmail.com>
2024-03-10 21:20:32 +0900

    올바른 이메일, 비밀번호 입력시 folder페이지 이동 기능 구현

--- src/login.js
+++ src/login.js
@@ -11,6 +11,8 @@ emailInput.addEventListener("focusout", function(){
     } else {
         if(!emailPattern.test(emailInput.value)) {
             emailError.textContent = "올바른 이메일 주소가 아닙니다."
+        } else {
+            emailError.textContent = ""
         }
     }
 });
@@ -19,6 +21,23 @@ emailInput.addEventListener("focusout", function(){
 pwInput.addEventListener("focusout", function(){
     if(pwInput.value.trim() === "") {
         pwError.textContent = "비밀번호를 입력해주세요";
+    } else {
+        pwError.textContent = ""
     }
 })
 
+//로그인 시도
+function attemptLogin() {
+    const validEmail = "test@codeit.com";
+    const validPW = "codeit101";
+
+    const emailInput = document.getElementById("emailInput").value;
+    const pwInput = document.getElementById("pwInput").value;
+
+    if(emailInput === validEmail && pwInput === validPW) {
+        document.location.href="/folder"
+    } else {
+        emailError.textContent = "이메일을 확인해 주세요"
+        pwError.textContent = "비밀번호를 확인해 주세요";
+    }
+}
