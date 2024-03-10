42891f4976503eef40885631b441354ec5c4cf7b
ehdus3255 <rlaehdus3255@naver.com>
2024-03-10 19:49:42 +0900

    feat: 자바스크립트 코드 수정

--- src/signin.js
+++ src/signin.js
@@ -3,37 +3,46 @@ function validateEmail(email){
     return validate.test(email);
 }
 
+
 const email = document.querySelector('#email');
 const password = document.querySelector('#password');
-const emailErrorMessage = document.querySelector('.email-error')
-const passwordErrorMessage = document.querySelector('.password-error')
+const emailErrorMessage = document.querySelector('.email-error');
+const passwordErrorMessage = document.querySelector('.password-error');
+const signinBtn = document.querySelector('.cta');
 
-const emailErrorEvent = function () {
-    if (email === '') {
+function emailErrorEvent () {
+    if (email.value === '') {
         email.classList.add('error-on');
         emailErrorMessage.textContent = "이메일을 입력해 주세요.";
+        return;
     }
 
-    if (!validateEmail(email)) {
+    if (!validateEmail(email.value)) {
         email.classList.add('error-on');
         emailErrorMessage.textContent = "올바른 이메일 주소가 아닙니다.";
+        return;
     }
 
     email.classList.remove('error-on');
     emailErrorMessage.textContent = "";
-};
+}
 
-const passwordErrorEvent = function () {
-    if (password === '') {
+email.addEventListener('focusout', emailErrorEvent);
+
+function passwordErrorEvent () {
+    if (password.value === '') {
         password.classList.add('error-on');
         passwordErrorMessage.textContent = "비밀번호를 입력해 주세요.";
+        return;
     }
 
     email.classList.remove('error-on');
     passwordErrorMessage.textContent = "";
-};
+}
 
-const signinEvent = function () {
+password.addEventListener('focusout', passwordErrorEvent);
+
+function signinEvent () {
     if (email.value === "test@codeit.com" && password === "codeit101") {
         location.href = "./folder.html";
     } else {
@@ -44,9 +53,13 @@ const signinEvent = function () {
     }
 }
 
-document.addEventListener('keydown', function(e){
+document.addEventListener('keydown', function(e) {
     if(e.key === 'Enter'){
       signinEvent(e);
     }
 });
 
+cta.addEventListener('click', function(e) {
+    e.preventDefault();
+    signinEvent(e);
+});
