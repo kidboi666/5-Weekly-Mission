1116a7203462c055fd25b88f4a488f4d143e4079
JunYoung <wnsdud96330@gmail.com>
2024-03-10 19:14:29 +0900

    Feat : Enter키로 로그인하기 및 비밀번호 보기 토글 기능 생성

--- static/script/index.js
+++ static/script/index.js
@@ -4,6 +4,10 @@ const emailTag = document.querySelector('.emailTag')
 // 비밀번호 input
 const pw = document.querySelector('.password');
 const passwordTag = document.querySelector('.passwordTag')
+// 로그인 button
+const loginBtn = document.querySelector('.loginBtn')
+// 비밀번호 눈 아이콘
+const eyesToggle = document.querySelector('.eyes')
 
 //emailInput focus-out시 이벤트 함수
 function idFocusOut(e) {
@@ -44,8 +48,61 @@ function passwordFocusOut(e) {
     }
 }
 
+// 로그인 함수
+function login(e) {
+    const testId = 'test@codeit.com';
+    const testPassword = 'codeit101';
+
+    if (email.value == testId && pw.value == testPassword) {
+        location.href='../folder.html';
+    }
+    else {
+        const loginCheck = document.querySelector('.login-check');
+
+        if(!loginCheck){
+            const notEmail = document.createElement('p');
+            const notPassword = document.createElement('p'); 
+    
+            notEmail.className = 'login-check';
+            notEmail.style.color = 'red';
+            notEmail.textContent = '이메일을 확인해주세요.';
+            emailTag.append(notEmail);
+    
+            notPassword.className = 'login-check';
+            notPassword.style.color = 'red';
+            notPassword.textContent = '비밀번호를 확인해주세요.';
+            passwordTag.append(notPassword);
+        }
+    }
+}
+
+//로그인 버튼 Enter키 적용
+function enterLogin (e) {
+    if (e.key === 'Enter') {
+      login();
+    }
+  }
+  
+//눈 아이콘 눌렀을 때 비밀번호 보이기
+function showPassword(e) {
+    if(eyesToggle.getAttribute('src') == '/static/img/noeyes.svg') {
+        eyesToggle.setAttribute('src', '/static/img/eyes.svg');
+        pw.setAttribute('type', 'text')
+    }
+    else {
+        eyesToggle.setAttribute('src', '/static/img/noeyes.svg'); 
+        pw.setAttribute('type', 'password')
+    }
+}
+
 // emailInput focus-out시 이벤트 함수 적용
 email.addEventListener('focusout', idFocusOut);
-
 // password focus-out시 이벤트 함수 적용
 pw.addEventListener('focusout', passwordFocusOut);
+// 로그인 이벤트 함수 적용
+loginBtn.addEventListener('click', login);
+// 로그인 시 Enter키 이벤트 함수 적용
+email.addEventListener('keypress', enterLogin);
+pw.addEventListener('keypress', enterLogin);
+// 눈 아이콘 클릭 시 password 보이게 만드는 이벤트 함수 적용
+eyesToggle.addEventListener('click', showPassword)
