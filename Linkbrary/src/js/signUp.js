dd3028957aee0f158e7471a2f92be40018e11341
bk-git-hub <bkprivate99@gmail.com>
2024-03-11 17:38:42 +0900

    feat: Add event listeners to input elements

--- Linkbrary/src/js/signUp.js
+++ Linkbrary/src/js/signUp.js
@@ -1,10 +1,16 @@
-import { handlePasswordToggleClick } from "./eventHandler";
+import {
+  handlePasswordToggleClick,
+  setFocusInOutListenerById,
+} from "./eventHandler";
 
 function initSignUp() {
   const pwToggleBtns = document.querySelectorAll(".pw-toggle-btn");
   for (btn of pwToggleBtns) {
     btn.addEventListener("click", handlePasswordToggleClick);
   }
+  setFocusInOutListenerById("#user-email");
+  setFocusInOutListenerById("#password");
+  setFocusInOutListenerById("#password-check");
 }
 
 initSignUp();
