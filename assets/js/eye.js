bb0b0fb5caf3bc755561ee4cc945660b303f1a70
juan0444 <dhwjs5267@gmail.com>
2024-03-11 19:11:04 +0900

    head 안태그 수정

--- assets/js/eye.js
+++ assets/js/eye.js
@@ -2,6 +2,7 @@ import { inputPassword } from '/assets/js/login.js';
 const eyeBtn = document.querySelector('.sign-pw__eye-btn');
 const eyeImg = document.querySelector('.eye-change');
 
+//반전되는 것은 이름에 toggle이 들어가면 좋음
 function eyeChange() {
   eyeImg.classList.toggle('eye-on');
   
