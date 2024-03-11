a91d2d3619e6f8418f29e48448c4b682a4a86faa
Bae uijin <baeuijin01@gmail.com>
2024-03-11 19:17:57 +0900

    Add password show & hide switching function to icon

--- signin/index.js
+++ signin/index.js
@@ -1,12 +1,15 @@
-const form = document.querySelector('.form')
-const id = document.querySelector('#username')
-const pwd = document.querySelector('#password')
-const btn = document.querySelector('.btn')
+const form = document.querySelector('.form');
+const id = document.querySelector('#username');
+const pwd = document.querySelector('#password');
+const btn = document.querySelector('.btn');
 const idCtr = document.querySelector('.username-container');
 const pwdCtr = document.querySelector('.password-container');
 const emailTest = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/;
+const pwdToggle = document.querySelector('.password-toggle');
 
 
+
+// focusout : 공란이거나 잘못된 이메일 형식 입력하면 에러메세지 추가
 function checkId() {
   if (id.value==='') {
     const message = document.createElement('div');
@@ -14,21 +17,15 @@ function checkId() {
     message.innerHTML='이메일을 입력해 주세요';
     idCtr.append(message);
     id.classList.toggle('error')
-  
-    // console.log('이메일을 입력해 주세요')
-    
+
   } else if (emailTest.test(id.value) === false) {
     const message = document.createElement('div');
     message.innerHTML = '올바른 이메일 주소가 아닙니다.';
     message.classList.add('text-error');
     idCtr.append(message);
     id.classList.add('error')
-
-    // console.log(id.value)
-    // console.log('올바른 이메일 주소가 아닙니다.')
   } 
 }
-
 
 function checkPwd() {
   if (pwd.value==='') {
@@ -37,11 +34,14 @@ function checkPwd() {
     message.classList.add('text-error');
     pwdCtr.append(message)
     pwd.classList.add('error')
-    
-    // console.log('비밀번호를 입력해 주세요')
   }
 }
 
+id.addEventListener('focusout', checkId);
+pwd.addEventListener('focusout', checkPwd);
+
+
+// focusin : 에러메세지 초기화
 function removeId() {
   const erase = idCtr.querySelector('.text-error')
   if (erase) {
@@ -59,13 +59,19 @@ function removePwd() {
   } 
 }
 
+id.addEventListener('focusin', removeId);
+pwd.addEventListener('focusin', removePwd);
 
+
+// click, keypress : 로그인 정보 전송
 function submit() {
   if (id.value==='test@codeit.com' && pwd.value === 'codeit101') {
     window.location.href = '/folder'
   }
-  // console.log('로그인')
+  id.value=''
+  pwd.value=''
 }
+
 
 function submitByEnter(e) {
   if (e.key === 'Enter') {
@@ -73,10 +79,23 @@ function submitByEnter(e) {
   }
 }
 
-id.addEventListener('focusout', checkId);
-pwd.addEventListener('focusout', checkPwd);
-id.addEventListener('focusin', removeId);
-pwd.addEventListener('focusin', removePwd);
 btn.addEventListener('click', submit)
-// form.addEventListener('keypress', submitByEnter);
+form.addEventListener('keypress', submitByEnter);
+
+
+// clikc : 눈 아이콘 토글 시 비밀번호 숨기기 & 보이기 기능 추가
+function togglePassword() {
+  if (pwd.getAttribute('type') ==='password') {
+    pwd.setAttribute('type', 'text');
+    pwdToggle.classList.remove('eye-slash');
+    pwdToggle.classList.add('eye');
+  } else {
+    pwd.setAttribute('type', 'password');
+    pwdToggle.classList.remove('eye');
+    pwdToggle.classList.add('eye-slash');
+  }
+}
+
+pwdToggle.addEventListener('click', togglePassword)
+
 
