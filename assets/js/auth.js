8a21df8543c06d776d1df1674b35a83ff0c7f792
daeun <qzsz797979@gmail.com>
2024-03-13 15:51:18 +0900

    className 변경 - notice -> error, auth-form -> form 등 클래스 이름 개선

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -6,11 +6,11 @@ const passwordConfirmEyeButton = document.querySelector(
   "#passwordConfirmEyeButton"
 );
 
-function createInputError(target, textContent) {
-  const notice = document.createElement("div");
-  notice.classList.add("auth-form__notice");
-  notice.textContent = textContent;
-  target.parentElement.after(notice);
+function createError(target, textContent) {
+  const error = document.createElement("div");
+  error.classList.add("error-message");
+  error.textContent = textContent;
+  target.parentElement.after(error);
   target.classList.add("input-border--red");
 }
 
@@ -19,7 +19,7 @@ function showErrorToWrite(e) {
     e.target.id === "email"
       ? "이메일을 입력해 주세요."
       : "비밀번호를 입력해 주세요.";
-  createInputError(e.target, textContent);
+  createError(e.target, textContent);
 }
 
 function checkEmailFormat(e) {
@@ -29,24 +29,24 @@ function checkEmailFormat(e) {
 
 function showErrorToWriteInEmailFormat(e) {
   const textContent = "올바른 이메일 주소가 아닙니다.";
-  const previousNotice = e.target.parentElement.nextElementSibling;
-  if (previousNotice) {
-    previousNotice.textContent = textContent;
+  const previousError = e.target.parentElement.nextElementSibling;
+  if (previousError) {
+    previousError.textContent = textContent;
   } else {
     createInputError(e.target, textContent);
   }
 }
 
 function checkInvalidInput(e) {
-  const checkPreviousNotice =
+  const isPreviousError =
     e.target.parentElement.parentElement.childNodes.length !== 5;
   if (!e.target.value) {
-    !checkPreviousNotice && showErrorToWrite(e);
+    !isPreviousError && showErrorToWrite(e);
   } else {
     if (e.target === email && !checkEmailFormat(e)) {
       showErrorToWriteInEmailFormat(e);
     } else {
-      if (checkPreviousNotice) {
+      if (isPreviousError) {
         e.target.parentElement.nextElementSibling.remove();
         e.target.classList.remove("input-border--red");
       }
@@ -54,7 +54,7 @@ function checkInvalidInput(e) {
   }
 }
 
-function login(e) {
+function handleLogin(e) {
   e.preventDefault();
   const adminEmail = "test@codeit.com";
   const adminPassword = "codeit101";
@@ -65,11 +65,11 @@ function login(e) {
   } else {
     if (!email.parentElement.nextElementSibling) {
       const textContent = "이메일을 확인해 주세요.";
-      createInputError(email, textContent);
+      createError(email, textContent);
     }
     if (!password.parentElement.nextElementSibling) {
       const textContent = "비밀번호를 확인해 주세요.";
-      createInputError(password, textContent);
+      createError(password, textContent);
     }
   }
 }
@@ -79,17 +79,17 @@ function togglePassword(e) {
   if (currentPassword.type === "password") {
     currentPassword.type = "text";
     e.currentTarget.innerHTML =
-      '<img class="auth-form__eye eye--on" src="/assets/img/eye_on.png" alt="eye on" />';
+      '<img class="form__eye on" src="/assets/img/eye_on.svg" alt="eye on" />';
   } else {
     currentPassword.type = "password";
     e.currentTarget.innerHTML =
-      '<img class="auth-form__eye eye--off" src="/assets/img/eye_off.png" alt="eye off" />';
+      '<img class="form__eye off" src="/assets/img/eye_off.svg" alt="eye off" />';
   }
 }
 
 email && email.addEventListener("focusout", checkInvalidInput);
 password && password.addEventListener("focusout", checkInvalidInput);
-signinForm && signinForm.addEventListener("submit", login);
+signinForm && signinForm.addEventListener("submit", handleLogin);
 passwordEyeButton && passwordEyeButton.addEventListener("click", togglePassword);
 passwordConfirmEyeButton &&
   passwordConfirmEyeButton.addEventListener("click", togglePassword);
