fa1f33934428e3fcad557e465a5da98160569c6c
Bae uijin <baeuijin01@gmail.com>
2024-03-13 21:43:53 +0900

    Replaced codes belongs to error message in signin page

--- signin/index.js
+++ signin/index.js
@@ -12,28 +12,30 @@ const pwdToggle = document.querySelector('.password-toggle');
 // focusout : 공란이거나 잘못된 이메일 형식 입력하면 에러메세지 추가
 function checkId() {
   if (id.value==='') {
-    const message = document.createElement('div');
-    message.classList.add('text-error');
-    message.innerHTML='이메일을 입력해 주세요';
-    idCtr.append(message);
-    id.classList.toggle('error')
+    // 메세지 보이게
+    const showMsg = idCtr.lastElementChild;
+    showMsg.style.display = 'block';
+    // 이메일 박스 빨간테두리 추가
+    id.classList.toggle('error');
 
   } else if (emailTest.test(id.value) === false) {
-    const message = document.createElement('div');
-    message.innerHTML = '올바른 이메일 주소가 아닙니다.';
-    message.classList.add('text-error');
-    idCtr.append(message);
-    id.classList.add('error')
+    // 메세지 보이게
+    const showMsg = idCtr.lastElementChild;
+    showMsg.style.display = 'block';
+    // 메세지 내용 바꿈
+    showMsg.innerHTML = '올바른 이메일 주소가 아닙니다.';
+    // 이메일 박스 빨간테두리 추가
+    id.classList.toggle('error');
   } 
 }
 
 function checkPwd() {
   if (pwd.value==='') {
-    const message = document.createElement('div');
-    message.innerHTML = '비밀번호를 입력해 주세요';
-    message.classList.add('text-error');
-    pwdCtr.append(message)
-    pwd.classList.add('error')
+    // 메세지 보이게
+    const showMsg = pwdCtr.lastElementChild;
+    showMsg.style.display = 'block';
+    // 이메일 박스 빨간테두리 추가
+    pwd.classList.toggle('error');
   }
 }
 
@@ -42,25 +44,28 @@ pwd.addEventListener('focusout', checkPwd);
 
 
 // focusin : 에러메세지 초기화
-function removeId() {
-  const erase = idCtr.querySelector('.text-error')
-  if (erase) {
-    erase.remove();
-    id.classList.remove('error')
-  } 
+function resetId() {
+  // 메세지 안보이게
+  const resetMsg = idCtr.lastElementChild
+  resetMsg.style.display = 'none'
+  // 메세지 내용 원상복구
+  resetMsg.innerHTML = '이메일을 입력해주세요.';
+  // 이메일 박스 테두리 원상복구
+  id.classList.remove('error')
+
 }
 
 
-function removePwd() {
-  const erase = pwdCtr.querySelector('.text-error')
-  if (erase) {
-    erase.remove();
-    pwd.classList.remove('error');
-  } 
+function resetPwd() {
+  // 메세지 안보이게
+  const resetMsg = pwdCtr.lastElementChild
+  resetMsg.style.display = 'none'
+  // 이메일 박스 테두리 원상복구
+  pwd.classList.remove('error')
 }
 
-id.addEventListener('focusin', removeId);
-pwd.addEventListener('focusin', removePwd);
+id.addEventListener('focusin', resetId);
+pwd.addEventListener('focusin', resetPwd);
 
 
 // click, keypress : 로그인 정보 전송
