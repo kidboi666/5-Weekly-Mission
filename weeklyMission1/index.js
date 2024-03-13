bf43f96b523f8f12bb99a88f77b1001f94a38b9f
Homin <coc1381@naver.com>
2024-03-13 18:49:00 +0900

    비밀번호 유효성 검사 기능 구현 완료

--- weeklyMission1/index.js
+++ weeklyMission1/index.js
@@ -1,10 +1,15 @@
 const signInEmailInput = document.querySelector('.signin-email-input');
-const signUpEmailInput = document.querySelector('.signup-email-input');
 const signInPasswordInput = document.querySelector('.signin-password-input');
+const signUpEmailInput = document.querySelector('.signup-email-input');
+const signUpPasswordInput = document.querySelector('.signup-password-input');
 const emailErrorMsg = document.querySelector('.email-error-msg');
+const signUpEmailErrorMsg = document.querySelector('.signup-email-error-msg');
+const signUpPasswordCheckInput = document.querySelector('.signup-password-check-input');
+const passwordCheckErrorMsg = document.querySelector('.password-check-error-msg');
 const passwordErrorMsg = document.querySelector('.password-error-msg');
 const signInLogin = document.querySelector('.signin-login');
 const passwordImg = document.querySelector('.password-img');
+const passwordImgCheck = document.querySelector('.password-img-check');
 const MEMBER_ID = "test@codeit.com";
 const MEMBER_PASSWORD = "codeit101";
 
@@ -26,28 +31,71 @@ function checkEmail(emailInput) {
     }
 }
 
+function checkEmailValid() {
+    const signInEmailInputValue = signInEmailInput.value;
+
+    if (!checkEmail(signInEmailInputValue)) {
+        emailErrorMsg.textContent = "올바른 이메일 주소가 아닙니다.";
+        signInEmailInput.style.border = "1px solid red";
+    } else {
+        emailErrorMsg.textContent = "";
+        signInEmailInput.style.border = "1px solid #9fa6b2";
+    }
+}
+
 function checkEmailDuplicate() {
     const signUpEmailInputValue = signUpEmailInput.value;
 
     if (signUpEmailInputValue === MEMBER_ID) {
-        emailErrorMsg.textContent = "이미 사용 중인 이메일입니다."
-        signInEmailInput.style.border = "1px solid red";
+        signUpEmailErrorMsg.textContent = "이미 사용 중인 이메일입니다.";
     }
-}
-
-function checkEmailValid() {
-    const signInEmailInputValue = signInEmailInput.value;
-    const isCheckEmail = checkEmail(signInEmailInputValue);
-
-    emailErrorMsg.textContent = isCheckEmail ? '' : "올바른 이메일 주소가 아닙니다.";
-    signInEmailInput.style.border = `1px solid ${isCheckEmail ? "#9fa6b2" : "red"}`;
 }
 
 function checkPasswordBlank() {
     const signInPasswordInputValue = signInPasswordInput.value;
 
-    passwordErrorMsg.textContent  = signInPasswordInputValue ? "" : "비밀번호를 입력해주세요.";
-    signInPasswordInput.style.border = `1px solid ${signInPasswordInputValue ? "#9fa6b2" : "red"}`;
+    if (!signInPasswordInputValue) {
+        passwordErrorMsg.textContent = "비밀번호를 입력해주세요.";
+        signInPasswordInput.style.border = "1px solid red";
+    } else {
+        passwordErrorMsg.textContent = "";
+        signInPasswordInput.style.border = "1px solid #9fa6b2";
+    }
+}
+
+function checkPassword(passwordInput) {
+    const passwordRegex = /^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{8,20}$/;
+
+    if (!passwordRegex.test(passwordInput)) {
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function checkPasswordValid() {
+    const signUpPasswordInputValue = signUpPasswordInput.value;
+
+    if (!checkPassword(signUpPasswordInputValue)) {
+        passwordErrorMsg.textContent = "비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.";
+        signUpPasswordInput.style.border = "1px solid red";
+    } else {
+        passwordErrorMsg.textContent = "";
+        signUpPasswordInput.style.border = "1px solid #9fa6b2";
+    }
+}
+
+function checkPasswordDuplicate() {
+    const signInPasswordInputValue = signInPasswordInput.value;
+    const signUpPasswordCheckInputValue = signUpPasswordCheckInput.value;
+
+    if (signInPasswordInputValue !== signUpPasswordCheckInputValue) {
+        passwordCheckErrorMsg.textContent = "비밀번호가 일치하지 않아요.";
+        signUpPasswordCheckInput.style.border = "1px solid red";
+    } else {
+        passwordCheckErrorMsg.textContent = "";
+        signUpPasswordCheckInput.style.border = "1px solid #9fa6b2";
+    }
 }
 
 function checkMember() {
@@ -65,12 +113,26 @@ function checkMember() {
 }
 
 function passwordToggle() {
-    if (passwordImg.getAttribute('src') === "./img/hidden.svg") {
+    if (passwordImg.getAttribute('alt') === "closed-eye") {
+        passwordImg.setAttribute('alt', "opened-eye");
         passwordImg.setAttribute('src', "./img/unhidden.svg");
         signInPasswordInput.setAttribute('type', 'text');
-    } else {
+    } else if (passwordImg.getAttribute('alt') === "opened-eye") {
+        passwordImg.setAttribute('alt', "closed-eye");
         passwordImg.setAttribute('src', "./img/hidden.svg");
         signInPasswordInput.setAttribute('type', 'password');
+    }
+}
+
+function passwordToggleCheck() {
+    if (passwordImgCheck.getAttribute('alt') === "opened-eye-second") {
+        passwordImgCheck.setAttribute('alt', "closed-eye-second");
+        passwordImgCheck.setAttribute('src', "./img/hidden.svg");
+        signUpPasswordCheckInput.setAttribute('type', 'password');
+    } else if (passwordImgCheck.getAttribute('alt') === "closed-eye-second") {
+        passwordImgCheck.setAttribute('alt', "opened-eye-second");
+        passwordImgCheck.setAttribute('src', "./img/unhidden.svg");
+        signUpPasswordCheckInput.setAttribute('type', 'text');
     }
 }
 
@@ -86,12 +148,16 @@ signInEmailInput.addEventListener('blur', checkEmailBlank);
 signInEmailInput.addEventListener('input', checkEmailBlank);
 signInEmailInput.addEventListener('keydown', pressEnterToLogin);
 
-signUpEmailInput.addEventListener('blur', checkEmailDuplicate);
-signUpEmailInput.addEventListener('input', checkEmailDuplicate);
-
 signInPasswordInput.addEventListener('blur', checkPasswordBlank);
 signInPasswordInput.addEventListener('input', checkPasswordBlank);
 signInPasswordInput.addEventListener('keydown', pressEnterToLogin);
 
 signInLogin.addEventListener('click', checkMember);
 passwordImg.addEventListener('click', passwordToggle);
+passwordImgCheck.addEventListener('click', passwordToggleCheck);
+
+signUpEmailInput.addEventListener('blur', checkEmailDuplicate);
+signUpEmailInput.addEventListener('input', checkEmailDuplicate);
+
+signUpPasswordCheckInput.addEventListener('input', checkPasswordDuplicate);
+signUpPasswordInput.addEventListener('input', checkPasswordValid);
