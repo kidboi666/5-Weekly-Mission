49ae6c7c8ed5db5c32bf1df7e3917e4ba446b142
kimmiso <vmffjrm@naver.com>
2024-03-14 20:04:05 +0900

    Refactor: join.js처럼 여러기능함수를 단일기능함수로 수정

--- js/signin.js
+++ js/signin.js
@@ -15,13 +15,10 @@ const users = [
 function emailErrorMessage () { // 이메일 에러 메세지
   switch (validateEmail()) {
     case 'emailEmpty':
-      errorInputAdd(signinEmail, '이메일을 입력해 주세요.');
-      break;
-    case 'emailReg':
-      removeInputMessage(signinEmail);
+      addInputMessage(signinEmail, '이메일을 입력해 주세요.');
       break;
     case 'emailError':
-      errorInputAdd(signinEmail, '올바른 이메일 주소가 아닙니다.');
+      addInputMessage(signinEmail, '올바른 이메일 주소가 아닙니다.');
       break;
     default:
       removeInputMessage(signinEmail);
@@ -31,8 +28,8 @@ function emailErrorMessage () { // 이메일 에러 메세지
 
 function passwordErrorMessage () { // 비밀번호 에러 메세지
   switch (validatePassword()) {
-    case 'passwordEmpty':
-      errorInputAdd(signinPassword, '비밀번호를 입력해 주세요.');
+    case 'pwEmpty':
+      addInputMessage(signinPassword, '비밀번호를 입력해 주세요.');
       break;
     default:
       removeInputMessage(signinPassword);
@@ -47,7 +44,7 @@ function validateEmail () { // 이메일
     return 'emailEmpty'
   } else if (regex.test(signinEmail.value.trim())) {
     validateObject.email = true;
-    return 'emailReg'
+    return ''
   } else {
     validateObject.email = false;
     return 'emailError'
@@ -57,14 +54,14 @@ function validateEmail () { // 이메일
 function validatePassword () { // 비밀번호
   if (signinPassword.value.trim() === '') {
     validateObject.password = false;
-    return 'passwordEmpty'
+    return 'pwEmpty'
   } else {
     validateObject.password = true;
     return ''
   }
 }
 
-function errorInputAdd (target, massage) { // error massage add
+function addInputMessage (target, massage) { // error massage add
   const parent = target.closest('.form__box');
   parent.classList.add('error');
   parent.querySelector('.error__text').innerText = massage;
@@ -81,15 +78,15 @@ function validateForm (e) { // form handler
   validateEmail();
   validatePassword();
   formActiveBtn();
-  
+
   const user = users.find(data => data.email === signinEmail.value.trim() && data.pw === signinPassword.value.trim() );
   if (user) {
     window.location.href='/folder.html';
   } else if (users[0].email === signinEmail.value.trim()) {
-    errorInputAdd(signinPassword,'비밀번호를 확인해 주세요.')
+    addInputMessage(signinPassword,'비밀번호를 확인해 주세요.')
   } else {
-    errorInputAdd(signinEmail,'이메일을 확인해 주세요.')
-    errorInputAdd(signinPassword,'비밀번호를 확인해 주세요.')
+    addInputMessage(signinEmail,'이메일을 확인해 주세요.')
+    addInputMessage(signinPassword,'비밀번호를 확인해 주세요.')
   }
 }
 
--- js/signup.js
+++ js/signup.js
@@ -1,8 +1,8 @@
 const signupForm = document.querySelector('.signup__wrap form');
 const signupFomeInput = document.querySelectorAll('.signup__form input');
-const signupEmail = document.querySelector('.signup__wrap #form__email');
-const signupPassword = document.querySelector('.signup__wrap #form__pw');
-const signupPasswordConfirm = document.querySelector('.signup__wrap #form__pw__confirm');
+const signupEmail = document.querySelector('#form__email');
+const signupPassword = document.querySelector('#form__pw');
+const signupPasswordConfirm = document.querySelector('#form__pw__confirm');
 const submitButton = document.querySelector('.signup__wrap .btn_signup');
 
 let validateObject = { // 활성화
@@ -11,57 +11,89 @@ let validateObject = { // 활성화
   passwordConfirm:false,
 }; 
 
+function emailErrorMessage () { // 이메일 에러 메세지
+  switch (validateEmail()) {
+    case 'emailEmpty':
+      addInputMessage(signupEmail, '이메일을 입력해 주세요.');
+      break;
+    case 'emailError':
+      addInputMessage(signupEmail, '올바른 이메일 주소가 아닙니다.');
+      break;
+    default:
+      removeInputMessage(signupEmail);
+      break;
+  }
+}
+
+function passwordErrorMessage () { // 비밀번호 에러 메세지
+  switch (validatePassword()) {
+    case 'pwEmpty':
+      addInputMessage(signupPassword, '비밀번호를 입력해 주세요.');
+      break;
+    default:
+      removeInputMessage(signupPassword);
+      break;
+  }
+}
+
+function passwordConfirmErrorMessage () {
+  switch (validatePasswordConfirm()) {
+    case 'pwConfirmEmpty':
+      addInputMessage(signupPasswordConfirm, '비밀번호 확인을 입력해 주세요.');
+      break;
+    case 'pwConfirmError':
+      addInputMessage(signupPasswordConfirm, '비밀번호가 다릅니다.');  
+      break;
+    default:
+      removeInputMessage(signupPasswordConfirm);
+      break;
+  }
+}
+
 function validateEmail () { // 이메일
   const regex = new RegExp('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$');
-  if(signupEmail.value.trim() !== '') {
-    if(regex.test(signupEmail.value.trim())) {
+  if (signupEmail.value.trim() === '') {
+    validateObject.email = false;
+    return 'emailEmpty'
+  } else if (regex.test(signupEmail.value.trim())) {
       validateObject.email = true;
-      errorInputRemove(signupEmail);
-    } else {
-      validateObject.email = false;
-      errorInputAdd(signupEmail, '올바른 이메일 주소가 아닙니다.');
-    }
+      return ''
   } else {
     validateObject.email = false;
-    errorInputAdd(signupEmail, '이메일을 입력해 주세요.');
+    return 'emailError'
   }
 }
 
 function validatePassword () { // 비밀번호
-  if(signupPassword.value.trim() !== '') {
-    if (signupPasswordConfirm.value.trim() !== ''){
-      validatePasswordConfirm();
-    }
-    validateObject.password = true;
-    errorInputRemove(signupPassword);
-  } else {
+  if (signupPassword.value.trim() === '') {
     validateObject.password = false;
-    errorInputAdd(signupPassword, '비밀번호를 입력해 주세요.');
+    return 'pwEmpty'
+  } else {
+    validateObject.password = true;
+    return ''
   }
 }
 
 function validatePasswordConfirm () { // 비밀번호 확인
-  if(signupPasswordConfirm.value.trim() !== '') {
-    if((signupPassword.value.trim() === signupPasswordConfirm.value.trim())) {
-      validateObject.passwordConfirm = true;
-      errorInputRemove(signupPasswordConfirm);
-      return false;
-    }
+  if (signupPasswordConfirm.value.trim() === '') {
     validateObject.passwordConfirm = false;
-    errorInputAdd(signupPasswordConfirm, '비밀번호가 다릅니다.');  
+    return 'pwConfirmEmpty'
+  } else if (signupPassword.value.trim() !== signupPasswordConfirm.value.trim()) {
+    validateObject.passwordConfirm = false;
+    return 'pwConfirmError'
   } else {
-    validateObject.passwordConfirm = false;
-    errorInputAdd(signupPasswordConfirm, '비밀번호 확인을 입력해 주세요.');
+    validateObject.passwordConfirm = true;
+    return ''
   }
 }
 
-function errorInputAdd (target, massage) { // error massage add
+function addInputMessage (target, massage) { // error massage add
   const parent = target.closest('.form__box');
   parent.classList.add('error');
   parent.querySelector('.error__text').innerText = massage;
 }
 
-function errorInputRemove (target) { // error massage remove
+function removeInputMessage (target) { // error massage remove
   const parent = target.closest('.form__box');
   parent.classList.remove('error');
   parent.querySelector('.error__text').innerText = ''
@@ -70,15 +102,15 @@ function errorInputRemove (target) { // error massage remove
 function focusOutInput (e) {
   const { target } = e;
   if(target.id === signupEmail.id) {
-    validateEmail();
+    emailErrorMessage();
   };
 
   if(target.id === signupPassword.id) {
-    validatePassword();
+    passwordErrorMessage ();
   };
 
   if(target.id === signupPasswordConfirm.id) {
-    validatePasswordConfirm();
+    passwordConfirmErrorMessage ();
   };
   formActiveBtn();
 }
