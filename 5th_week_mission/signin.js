5f020fab5a54584c6b8c3a1aed3a507c66e7f30b
DESKTOP-80GNNG0\WORK <ssjeond@naver.com>
2024-03-14 16:10:13 +0900

    fixed: renamed tag names for better readability

--- 5th_week_mission/signin.js
+++ 5th_week_mission/signin.js
@@ -1,6 +1,3 @@
-// 이메일 input에서 focus out 할 때, 값이 없을 경우 아래에 “이메일을 입력해 주세요.” 에러 메세지를 보입니다.
-// 이메일 input에서 focus out 할 때, 이메일 형식에 맞지 않는 값이 있는 경우 아래에 “올바른 이메일 주소가 아닙니다.” 에러 메세지를 보입니다
-
 //이메일 검사
 function validateEmail(email){
   const validate = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/;
@@ -10,7 +7,7 @@ function validateEmail(email){
 //입력칸이 빈칸이거나, 올바른 이메일 형식이 아닐때 에러문구 출력기능
 document.getElementById('email').addEventListener('focusout', function() {
   const email = this.value.trim();
-  const emailError = document.getElementById('email_error');
+  const emailError = document.getElementById('email-error');
 
   if (email === '') {
       emailError.textContent = '이메일을 입력해 주세요.';
@@ -23,7 +20,7 @@ document.getElementById('email').addEventListener('focusout', function() {
       this.classList.remove("emailErrorLine")
   }
 });
-const pwdError = document.getElementById('pwd_error');
+const pwdError = document.getElementById('pwd-error');
 //비밀번호 검사
 document.getElementById('password').addEventListener('focusout', function(){
   const password = this.value.trim();
@@ -40,7 +37,7 @@ document.getElementById('password').addEventListener('focusout', function(){
 });
 
 // Submit
-document.querySelector('.cta').addEventListener('click', submitSign);
+document.querySelector('.sign-button').addEventListener('click', submitSign);
 
 document.addEventListener('keydown', function submitByEnter(e) {
   if (e.key === 'Enter') {
@@ -57,8 +54,8 @@ function submitSign() {
     location.href = "/folder.html"; // 로그인 버튼 타입을 submit으로 하니 작동이 되지 않아, button type으로 설정함
   }else {
     // 입력된 이메일과 비밀번호가 다를 시에, 오류 메세지 출력
-    const emailError = document.getElementById('email_error');
-    const passwordError = document.getElementById('pwd_error');
+    const emailError = document.getElementById('email-error');
+    const passwordError = document.getElementById('pwd_-rror');
     const email = document.getElementById('email');
     const password = document.getElementById('password');
 
@@ -75,7 +72,7 @@ const passwordField = document.querySelector("#password");
 
 showPassword.addEventListener("click", function(){
   this.classList.toggle("eye-button-off");
-  const type = passwordField.getAttribute("type") 
-  === "password" ? "text" : "password" ;
+  const currentType = passwordField.getAttribute("type");
+  const type = currentt === "password" ? "text" : "password" ;
   passwordField.setAttribute("type", type);
 });
--- 5th_week_mission/signup.js
+++ 5th_week_mission/signup.js
@@ -3,31 +3,31 @@
 
 //이메일 검사
 function validateEmail(email){
-  const validate = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/;
-  return validate.test(email);
+  const emailRegrex = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/;
+  return emailRegrex.test(email);
 }
 
 //입력칸이 빈칸이거나, 올바른 이메일 형식이 아닐때 에러문구 출력기능
 document.getElementById('email').addEventListener('focusout', function() {
   const email = this.value.trim();
-  const emailError = document.getElementById('email_error');
+  const emailError = document.getElementById('email-error');
   if (email === '') {
       emailError.textContent = '이메일을 입력해 주세요.';
   } else if (!validateEmail(email)) {
       emailError.textContent = '올바른 이메일 주소가 아닙니다.';
   } else {
-      emailError.textContent = '   ';
+      emailError.textContent = '';
   }
 });
 
 //비밀번호 검사
 document.getElementById('password').addEventListener('focusout', function(){
   const password = this.value.trim();
-  const pwdError = document.getElementById('pwd_error');
+  const pwdError = document.getElementById('pwd-error');
   if (password === '') {
     pwdError.textContent = '비밀번호를 입력해 주세요.';
 } else {
-    pwdError.textContent = '   ';
+    pwdError.textContent = '';
 }
 });
 //비밀번호 확인 검사
@@ -37,10 +37,13 @@ document.getElementById('passwordRepeat').addEventListener('focusout', function(
   const pwdCheckerError = document.getElementById('pwdCheckerError');
   if (passwordRepeat === ''){
     pwdCheckerError.textContent = '비밀번호를 입력해 주세요.';
+    this.classList.add("pwdErrorLine");
   } else if (password  === passwordRepeat) {
-    pwdCheckerError.textContent = ' ';
+    pwdCheckerError.textContent = '';
+    this.classList.remove("pwdErrorLine");
   } else {
-    pwdCheckerError.textContent ='비밀번호가 다릅니다.'
+    pwdCheckerError.textContent ='비밀번호가 다릅니다.';
+    this.classList.add("pwdErrorLine");
   }  
 })
 
@@ -49,7 +52,7 @@ document.getElementById('passwordRepeat').addEventListener('focusout', function(
 // eye-button 클릭에 따라 eye-button on/off를 토글하고, password input type을 변경함 
 const showPassword = document.querySelector("#showPassword");
 const passwordField = document.querySelector("#password");
-const showPassword2 = document.querySelector("#showPassword2");
+const showPasswordCheck = document.querySelector("#showPasswordCheck");
 const passwordRepeatField = document.querySelector("#passwordRepeat");
 
 // 비밀번호 input 이벤트 핸들러
@@ -60,7 +63,7 @@ showPassword.addEventListener("click", function(){
   passwordField.setAttribute("type", type);
 });
 // 비밀번호 확인 input 이벤트 핸들러
-showPassword2.addEventListener("click", function(){
+showPasswordCheck.addEventListener("click", function(){
   this.classList.toggle("eye-button-off");
   const type = passwordRepeatField.getAttribute("type") 
   === "password" ? "text" : "password" ;
