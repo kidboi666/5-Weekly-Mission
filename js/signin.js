f0109d9ffd032fcf961db169b18a870d494216a7
kimmiso <vmffjrm@naver.com>
2024-03-14 19:27:13 +0900

    Refactor: 여러 기능을 수행하던 함수들을 단일 기능으로 함수 분리
    'emailErrorMessage','passwordErrorMessage'를 추가하여,
    'validateEmail','validatePassword'에서 에러메세지 기능을 따로
    분리하였습니다.

--- js/signin.js
+++ js/signin.js
@@ -1,7 +1,7 @@
 const signinForm = document.querySelector('.signin__wrap form');
 const signinFomeInput = document.querySelectorAll('.signin__wrap input');
-const signinEmail = document.querySelector('.signin__wrap #signin__email');
-const signinPassword = document.querySelector('.signin__wrap #signin__pw__input');
+const signinEmail = document.querySelector('#signin__email');
+const signinPassword = document.querySelector('#signin__pw__input');
 const submitButton = document.querySelector('.signin__wrap .btn_signin');
 let validateObject = { // 활성화
   email:false,
@@ -9,32 +9,58 @@ let validateObject = { // 활성화
 }; 
 
 const users = [
-  {email:'test@codeit.com', pw:'codeit101'},
+  { email:'test@codeit.com', pw:'codeit101' },
 ]
+
+function emailErrorMessage () { // 이메일 에러 메세지
+  switch (validateEmail()) {
+    case 'emailEmpty':
+      errorInputAdd(signinEmail, '이메일을 입력해 주세요.');
+      break;
+    case 'emailReg':
+      removeInputMessage(signinEmail);
+      break;
+    case 'emailError':
+      errorInputAdd(signinEmail, '올바른 이메일 주소가 아닙니다.');
+      break;
+    default:
+      removeInputMessage(signinEmail);
+      break;
+  }
+}
+
+function passwordErrorMessage () { // 비밀번호 에러 메세지
+  switch (validatePassword()) {
+    case 'passwordEmpty':
+      errorInputAdd(signinPassword, '비밀번호를 입력해 주세요.');
+      break;
+    default:
+      removeInputMessage(signinPassword);
+      break;
+  }
+}
 
 function validateEmail () { // 이메일
   const regex = new RegExp('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$');
-  if(signinEmail.value.trim() !== '') {
-    if(regex.test(signinEmail.value.trim())) {
-      validateObject.email = true;
-      errorInputRemove(signinEmail);
-    } else {
-      validateObject.email = false;
-      errorInputAdd(signinEmail, '올바른 이메일 주소가 아닙니다.');
-    }
+  if (signinEmail.value.trim() === '') {
+    validateObject.email = false;
+    return 'emailEmpty'
+  } else if (regex.test(signinEmail.value.trim())) {
+    validateObject.email = true;
+    return 'emailReg'
   } else {
     validateObject.email = false;
-    errorInputAdd(signinEmail, '이메일을 입력해 주세요.');
+    return 'emailError'
   }
 }
 
 function validatePassword () { // 비밀번호
-  if(signinPassword.value.trim() !== '') {
-    validateObject.password = true;
-    errorInputRemove(signinPassword);
-  } else {
+  if (signinPassword.value.trim() === '') {
     validateObject.password = false;
-    errorInputAdd(signinPassword, '비밀번호를 입력해 주세요.');
+    return 'passwordEmpty'
+  } else {
+    validateObject.password = true;
+    return ''
   }
 }
 
@@ -44,7 +70,7 @@ function errorInputAdd (target, massage) { // error massage add
   parent.querySelector('.error__text').innerText = massage;
 }
 
-function errorInputRemove (target) { // error massage remove
+function removeInputMessage(target) { // error massage remove
   const parent = target.closest('.form__box');
   parent.classList.remove('error');
   parent.querySelector('.error__text').innerText = ''
@@ -55,9 +81,12 @@ function validateForm (e) { // form handler
   validateEmail();
   validatePassword();
   formActiveBtn();
+  
   const user = users.find(data => data.email === signinEmail.value.trim() && data.pw === signinPassword.value.trim() );
-  if(user) {
+  if (user) {
     window.location.href='/folder.html';
+  } else if (users[0].email === signinEmail.value.trim()) {
+    errorInputAdd(signinPassword,'비밀번호를 확인해 주세요.')
   } else {
     errorInputAdd(signinEmail,'이메일을 확인해 주세요.')
     errorInputAdd(signinPassword,'비밀번호를 확인해 주세요.')
@@ -65,20 +94,20 @@ function validateForm (e) { // form handler
 }
 
 function focusOutInput (e) {
-  const { target } = e;
-  if(target.id === signinEmail.id) {
-    validateEmail();
+  const {target} = e;
+  if (target.id === signinEmail.id) {
+    emailErrorMessage();
   };
 
-  if(target.id === signinPassword.id) {
-    validatePassword();
+  if (target.id === signinPassword.id) {
+    passwordErrorMessage();
   };
   formActiveBtn();
 }
 
 function formActiveBtn () {
   const inputs = [validateObject.email,validateObject.password].every(data => data === true);
-  if(inputs) {
+  if (inputs) {
     submitButton.removeAttribute('disabled');
   } else {
     submitButton.setAttribute('disabled', 'disabled');
