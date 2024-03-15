cee8144f0385aa38b4b658fe25a0f95ba5e34d9f
hoonj170214 <134394435+hoonj170214@users.noreply.github.com>
2024-03-15 10:44:42 +0900

    [Part1 장성훈 week4] 이메일, 패스워드 검사 (#123)
    * first commit
    
    * 2주차 수정
    
    * 3주차 시작
    
    * 4주차 시작
    
    * 이메일, 패스워드 에러메세지 기능
    
    * focusout 이벤트 핸들러 수정
    
    * 폴더 구조변경
    
    * valid 파일 구조 변경
    
    * (index.js)let 변수를 const로 변경
    
    * 로그인, 회원가입 HTML, CSS 클래스네임 수정
    
    * 회원가입, 로그인 눈 position 수정
    
    * index.html classname 수정
    
    * 이메일, 패스워드 선택 수정(class -> type)
    
    * 에러 메세지 출력함수 수정
    
    * (signin.js) dom과 로직 분리

---
+++ login/signin.js
@@ -0,0 +1,61 @@
+/* 
+- 이메일 input에서 focus out 할 때, 값이 없을 경우 아래에 “이메일을 입력해 주세요.” 에러 메세지를 보입니다.
+- 비밀번호 input에서 focus out 할 때, 값이 없을 경우 아래에 “비밀번호를 입력해 주세요.” 에러 메세지를 보입니다.
+- 이메일 input에서 focus out 할 때, 이메일 형식에 맞지 않는 값이 있는 경우 아래에 “올바른 이메일 주소가 아닙니다.” 에러 메세지를 보입니다.
+- 이메일: test@codeit.com, 비밀번호: codeit101 으로 로그인 시도할 경우, “/folder” 페이지로 이동합니다.
+- 이외의 로그인 시도의 경우, 이메일 input 아래에 “이메일을 확인해 주세요.”, 비밀번호 input 아래에 “비밀번호를 확인해 주세요.” 에러 메세지를 보입니다.
+- input에 에러와 관련된 디자인은 좌측 상단의 Components 영역에 있는 디자인을 참고해 주세요.
+- 로그인 버튼 클릭 또는 Enter키 입력으로 로그인 실행돼야 합니다.
+*/
+
+// function createErrorMessage(message) {
+//   message.textContent = message;
+//   return message;
+// }
+
+function appendErrorMessage(errorContainer, message) {
+  errorContainer.innerHTML = message;
+}
+
+function showEmailErrorMessage(emailErrorMessage) {
+  if (emailInput.value === '') {
+    appendErrorMessage(emailErrorMessage, '이메일을 입력해주세요');
+  } else if (emailInput.value.indexOf('@') === -1) {
+    appendErrorMessage(emailErrorMessage, '올바른 이메일 주소가 아닙니다.');
+  } else {
+    appendErrorMessage(emailErrorMessage, '');
+  }
+}
+
+function showPasswordErrorMessage(passwordErrorMessage) {
+  if (passwordInput.value === '') {
+    appendErrorMessage(passwordErrorMessage, '비밀번호를 입력해주세요');
+  } else {
+    appendErrorMessage(passwordErrorMessage, '');
+  }
+}
+
+function validateEmail() {
+  showEmailErrorMessage(emailErrorMessage);
+}
+function validatePassword() {
+  showPasswordErrorMessage(passwordErrorMessage);
+}
+
+const emailInput = document.querySelector('input[type="email"]');
+const emailErrorMessage = document.querySelector(
+  '.form-group__error-message--email'
+);
+const passwordInput = document.querySelector('input[type="password"]');
+const passwordErrorMessage = document.querySelector(
+  '.form-group__error-message--password'
+);
+
+emailInput.addEventListener('focusout', validateEmail);
+passwordInput.addEventListener('focusout', validatePassword);
+
+document.addEventListener('keydown', function (event) {
+  if (event.key === 'Enter') {
+    event.preventDefault();
+  }
+});
