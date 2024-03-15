5e4e56da22903fbccb19fab402cc89f832baf648
GoldenHamsterK <kimdajin0928@gmail.com>
2024-03-15 15:32:13 +0900

    refactor: 코드리뷰 수정완료

--- signin.js
+++ signin.js
@@ -1,22 +1,27 @@
 
 const emailInputs = document.querySelector(`.email-input-group`);
 const passwordInputs = document.querySelector(`.password-input-group`);
-const singinBtn = document.querySelector(`#singin-btn`);
+const singinBtn = document.querySelector('#singin-btn');
 const emailErrorText = document.querySelector('.email-input-message');
 const emailInput = document.querySelector('.email-input');
 const passwordErrorText = document.querySelector('.password-input-message');
 const passwordInput = document.querySelector('.password-input');
+const EmptyEmailErrorMessage = '이메일을 입력해주세요.';
+const InvalidEmailErrorMessage = '올바른 이메일 형식이 아닙니다.';
+const EmptyPasswordErrorMessage =  '비밀번호를 입력해주세요.';
+const CheckEmailErrorMessage =  '이메일을 확인해주세요.';
+const CheckPasswordErrorMessage = '비밀번호를 확인해주세요.';
 
  emailInputs.addEventListener(`focusout`,(e) => {
-  emailText = e.target.value;
+  const emailText = e.target.value;
   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
   if (emailRegex.test(emailText) === false){
     if(emailText === ''){
-      emailErrorText.textContent = '이메일을 입력해주세요.';
+      emailErrorText.textContent = EmptyEmailErrorMessage;
       emailInput.classList.add('error-input');
     }
     else{
-      emailErrorText.textContent = '올바른 이메일 형식이 아닙니다.';
+      emailErrorText.textContent = InvalidEmailErrorMessage;
       emailInput.classList.add('error-input');
     }
   }
@@ -29,7 +34,7 @@ const passwordInput = document.querySelector('.password-input');
 passwordInputs.addEventListener(`focusout`,(e) => {
   passwordText = e.target.value;
   if(passwordText === ''){
-    passwordErrorText.textContent = '비밀번호를 입력해주세요.';
+    passwordErrorText.textContent = EmptyPasswordErrorMessage;
     passwordInput.classList.add('error-input');
   }
   else{
@@ -38,17 +43,19 @@ passwordInputs.addEventListener(`focusout`,(e) => {
   }
 });
 
+const user = { email : 'test@codeit.com', password : 'codeit101'}
+
 singinBtn.addEventListener('click',(e) => {
-  if(emailInput.value === `test@codeit.com` &&
-    passwordInput.value === `codeit101`){
+  if(emailInput.value === user.email &&
+    passwordInput.value === user.password){
     location.href = '/folder.html'
     }
   else{
-    emailErrorText.textContent = '이메일을 확인해주세요.';
-    emailInput.classList.add('error-input');
-    passwordErrorText.textContent = '비밀번호를 확인해주세요.';
-    passwordInput.classList.add('error-input');
     e.preventDefault();
+    emailErrorText.textContent = CheckEmailErrorMessage;
+    emailInput.classList.add('error-input');
+    passwordErrorText.textContent = CheckPasswordErrorMessage;
+    passwordInput.classList.add('error-input');
   }
 });
 
