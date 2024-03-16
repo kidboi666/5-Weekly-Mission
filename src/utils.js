0a9aaf64982dfda85de3d7bf7492bde5ee683d16
LeeSoonGu <rntnsl9280@naver.com>
2024-03-17 02:53:47 +0900

    회원가입 페이지 js 파일 추가

--- src/utils.js
+++ src/utils.js
@@ -18,6 +18,12 @@ export function isEmailValid(email) {
   return new RegExp(EMAIL_REGEX).test(email);
 }
 
+export function isPasswordValid(password) {
+  const isEightLettersOrMore = password.length >= 8;
+  const hasNumberAndCharacter = password.match(/[0-9]/g) && password.match(/[a-zA-Z]/gi);
+  return isEightLettersOrMore && hasNumberAndCharacter;
+}
+
 export function togglePassword(input, toggleButton) {
   if (input.getAttribute("type") === "password") {
     input.setAttribute("type", "text");
