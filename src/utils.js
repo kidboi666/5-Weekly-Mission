15210e6266d55a23165a8fb140ca92f61c0689a2
jangmoonwon <muneon96@naver.com>
2024-03-17 16:38:50 +0900

    feat: utils.js 파일 생성

---
+++ src/utils.js
@@ -0,0 +1,18 @@
+export function eyeToggleButton(input, toggleBtn) {
+  if (input.getAttribute("type") === "password") {
+    input.setAttribute("type", "text");
+    toggleBtn
+      .getElementsByTagName("img")[0]
+      .setAttribute("src", "./img/signin/eye-on.svg");
+    return;
+  }
+  input.setAttribute("type", "password");
+  toggleBtn
+    .getElementsByTagName("img")[0]
+    .setAttribute("src", "./img/signin/eye-off.svg");
+}
+
+export const TEST_USER = {
+  email: "test@codeit.com",
+  password: "codeit101",
+};
