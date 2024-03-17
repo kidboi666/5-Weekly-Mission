44b7b97e2bdfd176d49f6843c8672f35517186ba
juan0444 <dhwjs5267@gmail.com>
2024-03-17 16:31:10 +0900

    주석 삭제

--- assets/js/joinCheck.js
+++ assets/js/joinCheck.js
@@ -1,8 +1,3 @@
-//focus out
-// 3. 이메일 input에서 focus out 할 때, input 값이 test@codeit.com 일 경우, “이미 사용 중인 이메일입니다.” 에러 메세지를 보입니다.
-// 4. 비밀번호 input에서 focus out 할 때, 값이 8자 미만으로 있거나 문자열만 있거나 숫자만 있는 경우, “비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.” 에러 메세지를 보입니다.
-// 5. 비밀번호 input과 비밀번호 확인 input의 값이 다른 경우, 비밀번호 확인 input 아래에 “비밀번호가 일치하지 않아요.” 에러 메세지를 보입니다.
-
 import {   
   emailErrorMessage,
   pwErrorMessage,
--- assets/js/login.js
+++ assets/js/login.js
@@ -46,8 +46,3 @@ function handleSubmit(event) {
 }
 
 $form.addEventListener("submit", handleSubmit);
-
-// 공통
-// 1. 이메일 input에서 focus out 할 때, 값이 없을 경우 “이메일을 입력해주세요.” 에러 메세지를 보입니다.
-// 2. 이메일 input에서 focus out 할 때, 이메일 형식에 맞지 않는 값이 있는 경우 “올바른 이메일 주소가 아닙니다.” 에러 메세지를 보입니다.
-//----
