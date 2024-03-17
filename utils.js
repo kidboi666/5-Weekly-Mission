0491843b1cda52785535366b7cc77454fc65b502
박성재 <>
2024-03-17 21:23:01 +0900

    5주차 미션

--- utils.js
+++ utils.js
@@ -1,15 +1,16 @@
-export function emailErr(email, emailMsg, text) {
-  email.classList.add("err-input");
-  emailMsg.classList.add("err-on");
-  emailMsg.textContent = text;
+export function inputErr(input, inputMsg, text) {
+  input.classList.add("err-input");
+  inputMsg.classList.add("err-on");
+  inputMsg.textContent = text;
 }
-export function pwdErr(password, pwdMsg, text) {
-  password.classList.add("err-input");
-  pwdMsg.classList.add("err-on");
-  pwdMsg.textContent = text;
-}
+
 export function errChecked(type, typeMsg, text) {
   type.classList.remove("err-input");
   typeMsg.classList.remove("err-on");
   typeMsg.textContent = text;
+}
+
+export function validEmail() {
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  return emailRegex.test(email.value);
 }
