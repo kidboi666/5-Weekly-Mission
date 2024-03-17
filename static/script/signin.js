67ba7a2e247a74f0720aa0dbcec9663c0ad454de
JunYoung <wnsdud96330@gmail.com>
2024-03-17 18:27:55 +0900

    모듈화 및 로그인 회원가입 기능 구현

--- static/script/signin.js
+++ static/script/signin.js
@@ -1,17 +1,12 @@
-// 이메일 input
-const email = document.querySelector('.id');
-const emailTag = document.querySelector('.emailTag');
-// 비밀번호 input
-const pw = document.querySelector('.password');
-const passwordTag = document.querySelector('.passwordTag');
+import { email, emailTag, pw, passwordTag, testId, testPassword, emailCheck } from './common.js';
+
 // 로그인 button
 const loginBtn = document.querySelector('.loginBtn');
 // 비밀번호 눈 아이콘
-const eyesToggle = document.querySelector('.eyes');
+const passwordEyes = document.querySelector('.password-eyes');
 
 //emailInput focus-out시 이벤트 함수
 function idFocusOut(e) {
-    const emailCheck = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+$/;
     let emailNotice = document.querySelector('.email-notice');
 
     if(emailNotice) {
@@ -37,26 +32,23 @@ function idFocusOut(e) {
 // passwordInput focus-out시 이벤트 함수
 function passwordFocusOut(e) {
     let pwNotice = document.querySelector('.password-notice');
-    if (e.target.value=='') {
-        if (!pwNotice){
-            const passwordInput = document.createElement('p');
-            passwordInput.className = 'password-notice';
-            passwordInput.textContent = '비밀번호를 입력해주세요.';
-            passwordInput.style.color = 'red';
-            pw.style.borderColor = 'red';
-            passwordTag.append(passwordInput);
-        }
-    } else {
+    if(pwNotice) {
         pw.style.borderColor = 'gray';
         pwNotice.remove();
+    }
+
+    if (e.target.value=='') {
+        const passwordInput = document.createElement('p');
+        passwordInput.className = 'password-notice';
+        passwordInput.textContent = '비밀번호를 입력해주세요.';
+        passwordInput.style.color = 'red';
+        pw.style.borderColor = 'red';
+        passwordTag.append(passwordInput);
     }
 }
 
 // 로그인 함수
 function login(e) {
-    const testId = 'test@codeit.com';
-    const testPassword = 'codeit101';
-
     if (email.value == testId && pw.value == testPassword) {
         location.href='../folder.html';
     } else {
@@ -84,15 +76,15 @@ function enterLogin (e) {
     if (e.key === 'Enter') {
       login();
     }
-  }
+}
   
 //눈 아이콘 눌렀을 때 비밀번호 보이기
 function showPassword(e) {
-    if(eyesToggle.getAttribute('src') == '/static/img/noeyes.svg') {
-        eyesToggle.setAttribute('src', '/static/img/eyes.svg');
+    if(e.target.getAttribute('src') == '/static/img/noeyes.svg') {
+        e.target.setAttribute('src', '/static/img/eyes.svg');
         pw.setAttribute('type', 'text')
     } else {
-        eyesToggle.setAttribute('src', '/static/img/noeyes.svg'); 
+        e.target.setAttribute('src', '/static/img/noeyes.svg'); 
         pw.setAttribute('type', 'password')
     }
 }
@@ -107,4 +99,4 @@ loginBtn.addEventListener('click', login);
 email.addEventListener('keypress', enterLogin);
 pw.addEventListener('keypress', enterLogin);
 // 눈 아이콘 클릭 시 password 보이게 만드는 이벤트 함수 적용
-eyesToggle.addEventListener('click', showPassword)
+passwordEyes.addEventListener('click', showPassword)
--- static/script/signup.js
+++ static/script/signup.js
@@ -1,30 +1,23 @@
-// 이메일 input
-const email = document.querySelector('.id');
-const emailTag = document.querySelector('.emailTag');
-// 비밀번호 input
-const pw = document.querySelector('.password');
-const passwordTag = document.querySelector('.passwordTag');
+import { email, emailTag, pw, passwordTag, testId, testPassword, emailCheck } from './common.js';
 //비밀번호 확인 input
 const verifyPassword = document.querySelector('.verify-password');
 const verifyTag = document.querySelector('.verifyTag')
 // 회원가입 button
 const signupBtn = document.querySelector('.signupBtn');
 // 비밀번호 눈 아이콘
-const eyesToggle = document.querySelector('.eyes');
+const passwordEyes = document.querySelector('.password-eyes');
+const verifyEyes = document.querySelector(".verify-eyes");
 
-const testId = 'test@codeit.com';
-const testPassword = 'codeit101';
+
 
 //emailInput focus-out시 이벤트 함수
 function idFocusOut(e) {
-    const emailCheck = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+$/;
     let emailNotice = document.querySelector('.email-notice');
 
-    if(emailNotice) {
+    if (emailNotice) {
         email.style.borderColor = 'gray';
         emailNotice.remove();
     }
-
     const emailInput = document.createElement('p');
     emailInput.className = 'email-notice';
     emailInput.style.color = 'red';
@@ -48,36 +41,69 @@ function idFocusOut(e) {
 function passwordFocusOut(e) {
     const pwCheck = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
     let pwNotice = document.querySelector('.password-notice');
-    if (!pwCheck.test(e.target.value)) {
-        if (!pwNotice){
-            const passwordInput = document.createElement('p');
-            passwordInput.className = 'password-notice';
-            passwordInput.textContent = '비밀번호를 입력해주세요.';
-            passwordInput.style.color = 'red';
-            pw.style.borderColor = 'red';
-            passwordTag.append(passwordInput);
-        }
-    } else {
+
+    if (pwNotice) {
         pw.style.borderColor = 'gray';
         pwNotice.remove();
+    }
+
+    if (!pwCheck.test(e.target.value)) {
+        const passwordInput = document.createElement('p');
+        passwordInput.className = 'password-notice';
+        passwordInput.textContent = '비밀번호를 입력해주세요.';
+        passwordInput.style.color = 'red';
+        pw.style.borderColor = 'red';
+        passwordTag.append(passwordInput);
     }
 }
 
 function passwordVerify(e) {
     const verifyNotice = document.querySelector('.verify-notice');
-
-    if (pw.value != verifyPassword.value){
-        if(!verifyNotice) {
-            const verifyInput = document.createElement('p');
-            verifyInput.className = 'verify-notice';
-            verifyInput.textContent = '비밀번호가 일치하지 않아요.';
-            verifyInput.style.color = 'red';
-            verifyPassword.style.borderColor = 'red';
-            verifyTag.append(verifyInput);
-        }
-    } else {
+    if (verifyNotice) {
         verifyPassword.style.borderColor = 'gray';
         verifyNotice.remove();
+    }
+
+    if (pw.value != verifyPassword.value){
+        const verifyInput = document.createElement('p');
+        verifyInput.className = 'verify-notice';
+        verifyInput.textContent = '비밀번호가 일치하지 않아요.';
+        verifyInput.style.color = 'red';
+        verifyPassword.style.borderColor = 'red';
+        verifyTag.append(verifyInput);
+    }
+}
+
+function signup(e) {
+    const findNotice = document.querySelectorAll('.email-notice, .password-notice, .verify-notice');
+    if (findNotice.length === 0) {
+        location.href='../folder.html';
+    }
+}
+
+function enter (e) {
+    if (e.key === 'Enter') {
+        signup();
+    }
+}
+
+//눈 아이콘 눌렀을 때 비밀번호 보이기
+function showPassword(e) {
+    let inputField;
+    if(e.target === passwordEyes) {
+        inputField = pw;
+    } else if(e.target === verifyEyes) {
+        inputField = verifyPassword;
+    }
+
+    if(inputField) {
+        if(e.target.getAttribute('src') == '/static/img/noeyes.svg') {
+            e.target.setAttribute('src', '/static/img/eyes.svg');
+            inputField.setAttribute('type', 'text');
+        } else {
+            e.target.setAttribute('src', '/static/img/noeyes.svg'); 
+            inputField.setAttribute('type', 'password');
+        }
     }
 }
 
@@ -89,4 +115,16 @@ email.addEventListener('focusout', idFocusOut);
 pw.addEventListener('focusout', passwordFocusOut);
 
 // 비밀번호 input와 비밀번호 확인 input 태그의 value값 확인 이벤트 함수 적용
-verifyPassword.addEventListener('focusout', passwordVerify)
+verifyPassword.addEventListener('focusout', passwordVerify);
+
+// 회원가입 시 문제가 있는 input태그에 알림 기능
+signupBtn.addEventListener('click', signup);
+
+// 회원가입 시 Enter 클릭 이벤트 함수 적용
+email.addEventListener('keypress', enter);
+pw.addEventListener('keypress', enter);
+verifyPassword.addEventListener('keypress', enter);
+
+// 눈 아이콘 클릭 시 password 보이게 만드는 이벤트 함수 적용
+passwordEyes.addEventListener('click', showPassword);
+verifyEyes.addEventListener('click', showPassword);
