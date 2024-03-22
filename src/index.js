a22c0a0f335eb29b0d142171e62fecaf08426ace
SeungRyeolBaek <bsr20030507@g.skku.edu>
2024-03-22 17:54:39 +0900

    Global Style 파일 분리

--- src/index.js
+++ src/index.js
@@ -1,23 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createGlobalStyle } from "styled-components";
+import GlobalStyle from "./GlobalStyle";
 
-/**
- *@description 전역 설정
-*/
-const GlobalStyle = createGlobalStyle`
-* {
-  box-sizing: border-box;
-}
 
-/* 클릭으로 동작 -> 커서모양 변경으로 알림 */
-a {
-  cursor: pointer;
-}
-button {
-  cursor: pointer;
-}
-`;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
