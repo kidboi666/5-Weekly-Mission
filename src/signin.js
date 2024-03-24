5208b52ce52a531b344dc7c60cee1b46b9bac11d
ehdus3255 <rlaehdus3255@naver.com>
2024-03-24 22:45:00 +0900

    6주차 위클리 미션 적용

--- src/signin.js
+++ src/signin.js
@@ -3,7 +3,6 @@ import {
   removeInputError,
   isEmailValid,
   togglePassword,
-  TEST_USER,
 } from "./utils.js";
 
 const emailInput = document.querySelector("#email");
@@ -45,19 +44,32 @@ passwordToggleButton.addEventListener("click", () =>
 
 const signForm = document.querySelector("#form");
 signForm.addEventListener("submit", submitForm);
-function submitForm(event) {
+async function submitForm(event) {
   event.preventDefault();
 
-  const isTestUser =
-    emailInput.value === TEST_USER.email && passwordInput.value === TEST_USER.password;
+  try {
+    const response = await fetch("https://bootcamp-api.codeit.kr/api/sign-in", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify ({
+        email: emailInput.value,
+        password: passwordInput.value,
+      }),
+    });
 
-  if (isTestUser) {
-    location.href = "/folder";
-    return;
+    const user = await response.json();
+
+    if (user.data) {
+      location.href = "../folder.html";
+      return;
+    }
+  } catch {
+    setInputError({ input: emailInput, errorMessage: emailErrorMessage }, "이메일을 확인해주세요.");
+    setInputError(
+      { input: passwordInput, errorMessage: passwordErrorMessage },
+      "비밀번호를 확인해주세요."
+    );
   }
-  setInputError({ input: emailInput, errorMessage: emailErrorMessage }, "이메일을 확인해주세요.");
-  setInputError(
-    { input: passwordInput, errorMessage: passwordErrorMessage },
-    "비밀번호를 확인해주세요."
-  );
 }
