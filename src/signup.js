6494f0a30b728b2abf66fc476f2e2bb053f0f149
이보미 <leebomi@ibomiui-MacBookAir.local>
2024-03-24 18:33:36 +0900

    주석제거

--- src/signup.js
+++ src/signup.js
@@ -1,69 +1,73 @@
-const input = document.querySelector('.sign-input-email');
-const emailError = document.querySelector('.email-error');
-const input2 = document.querySelector('.sign-input-password');
-const pwError = document.querySelector('.pw-error');
-const input3 = document.querySelector('.sign-input-password-check');
-const pwCheckError = document.querySelector('.pw-check-error');
-const form = document.querySelector('.sign-form');
+const input = document.querySelector(".sign-input-email");
+const emailError = document.querySelector(".email-error");
+const input2 = document.querySelector(".sign-input-password");
+const pwError = document.querySelector(".pw-error");
+const input3 = document.querySelector(".sign-input-password-check");
+const pwCheckError = document.querySelector(".pw-check-error");
+const form = document.querySelector(".sign-form");
 
-input.addEventListener('focusout', function(e) {
-  if (input.value === "") {
-    emailError.innerHTML= "이메일을 입력해주세요.";
+input.addEventListener("focusout", function (e) {
+  if (!e.target.value) {
+    emailError.innerHTML = "이메일을 입력해주세요.";
     emailError.style.display = "block";
-    input.style.border = "1px solid #FF5B56";
-  } else if (emailCheck(input.value) === false) {
-    emailError.innerHTML= "올바른 이메일 주소가 아닙니다.";
+    e.target.style.border = "1px solid #FF5B56";
+  } else if (emailCheck(e.target.value) === false) {
+    emailError.innerHTML = "올바른 이메일 주소가 아닙니다.";
     emailError.style.display = "block";
-    input.style.border = "1px solid #FF5B56";
-  } else if (input.value === "test@codeit.com") {
-    emailError.innerHTML= "이미 사용 중인 이메일입니다.";
+    e.target.style.border = "1px solid #FF5B56";
+  } else if (e.target.value === "test@codeit.com") {
+    emailError.innerHTML = "이미 사용 중인 이메일입니다.";
     emailError.style.display = "block";
-    input.style.border = "1px solid #FF5B56";
+    e.target.style.border = "1px solid #FF5B56";
   } else {
-    emailError.innerHTML= "";
+    emailError.innerHTML = "";
     emailError.style.display = "none";
-    input.style.border = "1px solid #CCD5E3";
+    e.target.style.border = "1px solid #CCD5E3";
   }
 });
 
-function emailCheck(email_address){     
-	email_regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
-	if(!email_regex.test(email_address)){ 
-		return false; 
-	}else {
-		return true;
-	}
+function emailCheck(email_address) {
+  email_regex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/i;
+  if (!email_regex.test(email_address)) {
+    return false;
+  } else {
+    return true;
+  }
 }
 
-input2.addEventListener('focusout', function(e) {
+input2.addEventListener("focusout", function (e) {
   let regPass = /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}$/;
   if (!regPass.test(input2.value)) {
-    pwError.innerHTML="비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.";
+    pwError.innerHTML = "비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.";
     pwError.style.display = "block";
     input2.style.border = "1px solid #FF5B56";
   } else {
-    pwError.innerHTML="";
-    pwError.style.display = "none"
+    pwError.innerHTML = "";
+    pwError.style.display = "none";
     input2.style.border = "1px solid #CCD5E3";
   }
 });
 
-input3.addEventListener('focusout', function(e) {
+input3.addEventListener("focusout", function (e) {
   if (input2.value !== input3.value) {
-    pwCheckError.innerHTML="비밀번호가 일치하지 않아요.";
+    pwCheckError.innerHTML = "비밀번호가 일치하지 않아요.";
     pwCheckError.style.display = "block";
     input3.style.border = "1px solid #FF5B56";
   } else {
-    pwCheckError.innerHTML="";
+    pwCheckError.innerHTML = "";
     pwCheckError.style.display = "none";
     input3.style.border = "1px solid #CCD5E3";
   }
 });
 
-form.addEventListener('submit', function(e) {
+form.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  if (emailError.innerHTML==="" && pwError.innerHTML==="" && pwCheckError.innerHTML==="") {
-    window.location.href='/folder.html'; 
+  if (
+    emailError.innerHTML === "" &&
+    pwError.innerHTML === "" &&
+    pwCheckError.innerHTML === ""
+  ) {
+    window.location.href = "/folder.html";
   }
 });
