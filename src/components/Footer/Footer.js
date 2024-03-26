cb23c0d5ee304a3d71cd3181564ccd0988b21382
bk-git-hub <bkprivate99@gmail.com>
2024-03-26 14:42:05 +0900

    style: Apply style to footer component

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,16 +2,21 @@ import facebookLogo from '../../assets/images/facebook_logo.svg';
 import twitterLogo from '../../assets/images/twitter_logo.svg';
 import youtubeLogo from '../../assets/images/youtube_logo.svg';
 import instagramLogo from '../../assets/images/instagram_logo.svg';
+import './Footer.css';
 export default function Footer() {
   return (
-    <footer>
-      <div class='footer-bar'>
-        <div class='copyright'>©codeit - 2023</div>
-        <div class='policy-bar'>
-          <a href='#'>Privacy Policy</a>
-          <a href='#'>FAQ</a>
+    <footer className='main-footer'>
+      <div className='footer-bar'>
+        <div className='copyright'>©codeit - 2023</div>
+        <div className='policy-bar'>
+          <a href='#' className='footer-link'>
+            Privacy Policy
+          </a>
+          <a href='#' className='footer-link'>
+            FAQ
+          </a>
         </div>
-        <div class='info-link-bar'>
+        <div className='info-link-bar'>
           <ul>
             <li>
               <a href='#' target='_blank' rel='noreferrer noopener'>
