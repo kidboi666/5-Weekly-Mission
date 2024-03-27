37006c7ca21109823f568097fd7529ed8fddc6e4
이보미 <leebomi@ibomiui-MacBookAir.local>
2024-03-28 00:45:11 +0900

    기본 레이아웃 구조

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <div>nav</div>
+      <div>header</div>
+      <div className="body">
+        <div className="search-form"></div>
+        <div>
+          <div>card '반복문 map'</div>
+        </div>
+      </div>
+      <div>footer</div>
+    </div>
+  );
+}
+
+export default App;
