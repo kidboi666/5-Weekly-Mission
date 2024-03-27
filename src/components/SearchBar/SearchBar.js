034251853312f407aa8862fc65273ea6754f6a13
bk-git-hub <bkprivate99@gmail.com>
2024-03-27 14:12:49 +0900

    style: Apply style to SearchBar component

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,15 +1,19 @@
+import './SearchBar.css';
+import searchIcon from '../../assets/images/search_icon.svg';
+
 export default function SearchBar({ onSubmit }) {
   return (
-    <div className='search-bar'>
-      <form onSubmit={onSubmit}>
-        <input
-          type='text'
-          name='search-input'
-          id='search-link'
-          placeholder='링크를 검색하세요'
-          className='search-input'
-        />
-      </form>
-    </div>
+    <form onSubmit={onSubmit} className='search-bar'>
+      <button className='search-button' type='submit'>
+        <img src={searchIcon} alt='Search Icon' />
+      </button>
+      <input
+        type='text'
+        name='search-input'
+        id='search-link'
+        placeholder='링크를 검색하세요'
+        className='search-input'
+      />
+    </form>
   );
 }
