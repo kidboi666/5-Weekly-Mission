40a6d907f36a787b8ad59e8d289a7870c745d34c
yd170214 <sh20170214@gmail.com>
2024-03-30 03:53:32 +0900

    Header 컴포넌트 position fixed 수정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,18 +3,26 @@ import logo from '../assets/Linkbrary.svg';
 import userProfile from '../assets/profile_img.svg';
 import styled from 'styled-components';
 
+const Wrapper = styled.div`
+  top: 0;
+  width: 100%;
+  position: fixed;
+`;
+
 const Container = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
   background-color: #f0f6ff;
-  padding: 1.5rem 12.5rem;
+  padding: 1rem 12.5rem;
 
   @media screen and (max-width: 1023px) {
-    padding: 2rem;
+    padding-right: 2rem;
+    padding-left: 2rem;
   }
   @media screen and (max-width: 767px) {
-    padding: 1rem 2rem;
+    padding-right: 1rem;
+    padding-left: 1rem;
   }
 `;
 
@@ -43,16 +51,18 @@ const UserProfile = styled.div`
 function Header() {
   return (
     <header>
-      <Container>
-        <Logo href='../pages/main'>
-          <img src={logo} alt='로고' />
-        </Logo>
-        <UserProfile>
-          <img src={userProfile} alt='유저 아이콘' />
-          {/* TODO : 이메일은 api를 통해서 가져온 결과여야 함. */}
-          <p>유저 이메일</p>
-        </UserProfile>
-      </Container>
+      <Wrapper>
+        <Container>
+          <Logo href='../pages/main'>
+            <img src={logo} alt='로고' />
+          </Logo>
+          <UserProfile>
+            <img src={userProfile} alt='유저 아이콘' />
+            {/* TODO : 이메일은 api를 통해서 가져온 결과여야 함. */}
+            <p>유저 이메일</p>
+          </UserProfile>
+        </Container>
+      </Wrapper>
     </header>
   );
 }
