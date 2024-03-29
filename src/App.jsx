cdb42fa9c6f551c3abeffebc77f227f854014647
yup299 <yup299@gmail.com>
2024-03-30 02:40:43 +0900

    feat : Search컴포넌트를 Card컴포넌트와 함께 Content컴포넌트안으로 이동

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import './App.css';
 import Nav from './components/Nav.jsx';
 import Header from './components/Header.jsx';
-import Search from './components/Search.jsx';
+import Content from './components/Content.jsx';
 
 function App() {
   return (
     <>
       <Nav />
       <Header />
-      <Search />
+      <Content />
     </>
   );
 }
---
+++ src/components/Content.jsx
@@ -0,0 +1,13 @@
+import Search from './Search';
+import Card from './Card';
+
+function Content() {
+  return (
+    <>
+      <Search></Search>
+      <Card></Card>
+    </>
+  );
+}
+
+export default Content;
