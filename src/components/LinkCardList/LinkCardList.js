9f4d7bafdd75c4b8635371fc0ccb5548fdfb1259
bk-git-hub <bkprivate99@gmail.com>
2024-03-29 15:32:54 +0900

    refactor: Use constants in LinkCardList component

--- src/components/LinkCardList/LinkCardList.js
+++ src/components/LinkCardList/LinkCardList.js
@@ -2,12 +2,16 @@ import LinkCard from '../LinkCard/LinkCard';
 import SearchBar from '../SearchBar/SearchBar';
 import './LinkCardList.css';
 
+const STYLE_LINK_CARD_LIST_CONTAINER_CLASS_NAME = 'link-card-list-container';
+const STYLE_LINK_CARD_LIST_CLASS_NAME = 'link-card-list';
+const STYLE_CONTENT_WRAPPER_CLASS_NAME = 'content-wrapper';
+
 export default function LinkCardList({ items, searchOnSubmit }) {
   return (
-    <div className='link-card-list-container'>
-      <div className='content-wrapper'>
+    <div className={STYLE_LINK_CARD_LIST_CONTAINER_CLASS_NAME}>
+      <div className={STYLE_CONTENT_WRAPPER_CLASS_NAME}>
         <SearchBar onSubmit={searchOnSubmit} />
-        <ul className='link-card-list'>
+        <ul className={STYLE_LINK_CARD_LIST_CLASS_NAME}>
           {items.map((item) => (
             <li key={item.id}>
               <LinkCard linkCardInfo={item} />
