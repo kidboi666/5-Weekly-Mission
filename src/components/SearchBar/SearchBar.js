cdecedb6a853ab040492feb2203313649c97c874
bk-git-hub <bkprivate99@gmail.com>
2024-03-29 15:27:19 +0900

    refactor: Use constants in SearchBar component

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,18 +1,26 @@
 import './SearchBar.css';
 import searchIcon from '../../assets/images/search_icon.svg';
 
+const STYLE_SEARCH_BAR_CLASS_NAME = 'search-bar';
+const STYLE_SEARCH_BUTTON_CLASS_NAME = 'search-button';
+const STYLE_SEARCH_INPUT_CLASS_NAME = 'search-input';
+
+export const SEARCH_INPUT_ID = 'search-link';
+const SEARCH_INPUT_PLACEHOLDER = '링크를 검색하세요';
+const SEARCH_INPUT_ICON_ALT = 'Search Icon';
+
 export default function SearchBar({ onSubmit }) {
   return (
-    <form onSubmit={onSubmit} className='search-bar'>
-      <button className='search-button' type='submit'>
-        <img src={searchIcon} alt='Search Icon' />
+    <form onSubmit={onSubmit} className={STYLE_SEARCH_BAR_CLASS_NAME}>
+      <button className={STYLE_SEARCH_BUTTON_CLASS_NAME} type="submit">
+        <img src={searchIcon} alt={SEARCH_INPUT_ICON_ALT} />
       </button>
       <input
-        type='text'
-        name='search-input'
-        id='search-link'
-        placeholder='링크를 검색하세요'
-        className='search-input'
+        type="text"
+        name={SEARCH_INPUT_ID}
+        id={SEARCH_INPUT_ID}
+        placeholder={SEARCH_INPUT_PLACEHOLDER}
+        className={STYLE_SEARCH_INPUT_CLASS_NAME}
       />
     </form>
   );
