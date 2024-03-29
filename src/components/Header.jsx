1716e2ce39787f68575cd81fdb71c54002e9561a
yup299 <yup299@gmail.com>
2024-03-30 05:32:49 +0900

    feat : 폴더 소유자의 프로필 보이게 변경
    “/api/sample/folder” 통신후 값을 가져와  헤더에 보이게 변경

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,21 @@
-import header_logo from '../assets/header.svg';
+import { useState, useEffect } from 'react';
 import '../styles/Header.css';
+import fetchData from './apis/GetApi.jsx';
 
 function Header() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    fetchData('api/sample/folder')
+      .then((fetchedData) => setData(fetchedData))
+      .catch((error) => console.error(error));
+  }, []);
+
   return (
     <header className="Header">
-      <img src={header_logo} alt="Header Logo" className="Header__img" />
-      <p className="Header__text">@코드잇</p>
-      <h2 className="Header__title">⭐ 즐겨찾기</h2>
+      <img src={data.folder.owner.profileImageSource} alt="Header Logo" className="Header__img" />
+      <p className="Header__text">{data.folder.owner.name}</p>
+      <h2 className="Header__title">{data.folder.name}</h2>
     </header>
   );
 }
