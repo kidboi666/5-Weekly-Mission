1c6f069bdc78666cbda933539d2127fae7a78ab1
bk-git-hub <bkprivate99@gmail.com>
2024-03-29 14:51:07 +0900

    refactor: Use constants in Account component

--- src/components/Account/Account.js
+++ src/components/Account/Account.js
@@ -1,14 +1,19 @@
 import './Account.css';
+const STYLE_ACCOUNT_CLASS_NAME = 'account';
+const STYLE_USER_PROFILE_IMG_CLASS_NAME = 'user-profile-img';
+const STYLE_USER_EMAIL_CLASS_NAME = 'user-email';
+
+const USER_PROFILE_IMG_ALT = 'User Profile';
 
 export default function Account({ profileImgSource, userEmail }) {
   return (
-    <div className='account'>
+    <div className={STYLE_ACCOUNT_CLASS_NAME}>
       <img
         src={profileImgSource}
-        alt='User Profile'
-        className='user-profile-img'
+        alt={USER_PROFILE_IMG_ALT}
+        className={STYLE_USER_PROFILE_IMG_CLASS_NAME}
       />
-      <span className='user-email'>{userEmail}</span>
+      <span className={STYLE_USER_EMAIL_CLASS_NAME}>{userEmail}</span>
     </div>
   );
 }
