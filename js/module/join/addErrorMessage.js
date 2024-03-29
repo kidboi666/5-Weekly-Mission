ab2e35e662944325f4a3caabcab0fa517c207688
kimmiso <vmffjrm@naver.com>
2024-03-29 11:38:30 +0900

    Feat:초기화 작업

--- js/module/join/addErrorMessage.js
+++
@@ -1,5 +0,0 @@
-function addInputMessage (target, massage) { // error massage add
-  const parent = target.closest('.form__box');
-  parent.classList.add('error');
-  parent.querySelector('.error__text').innerText = massage;
-}
--- js/module/join/validation.js
+++
@@ -1,9 +0,0 @@
-// 이메일 유효성 검사 함수
-function validateEmail(email) {
-  return new RegExp('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$').test(email);
-}
-
-// 비밀번호 유효성 검사 함수
-function validatePassword(password) {
-  return new RegExp('^(?=.*[a-zA-Z])(?=.*[0-9]).{8,25}$').test(password);
-}
