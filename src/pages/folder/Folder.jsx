32e39186b0ffab215c180b84549ee376a4d009e1
yd170214 <sh20170214@gmail.com>
2024-03-30 03:54:33 +0900

    FolderInfo 컴포넌트로 분리

--- src/pages/folder/Folder.jsx
+++ src/pages/folder/Folder.jsx
@@ -1,18 +1,14 @@
-import { useState } from 'react';
 import Header from '../../components/Header';
 import Card from '../../components/Card';
 import Input from '../../components/Input';
 import Footer from '../../components/Footer';
+import FolderInfo from '../../components/FolderInfo';
 
 function Folder() {
   return (
-    <div>
+    <>
       <Header />
-      <div>
-        <img className='folder__user-profil-img' alt='유저 프로필 아이콘' />
-        <p className='folder__user-name'>@코드잇</p>
-        <p className='folder__name'>⭐️ 즐겨찾기</p>
-      </div>
+      <FolderInfo />
       <main>
         <Input />
         <Card />
@@ -26,7 +22,7 @@ function Folder() {
         <Card />
       </main>
       <Footer />
-    </div>
+    </>
   );
 }
 
