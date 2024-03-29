44a79c5c88cc166bfce2360dbd9165c46af88a3b
yd170214 <sh20170214@gmail.com>
2024-03-30 00:27:24 +0900

    카드 컴포넌트 마크업

---
+++ src/components/Card.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Card() {
+  return (
+    <div>
+      <img alt='대표이미지' />
+      <p>지난시간</p>
+      <p>콘텐츠 내용</p>
+      <p>게시 날짜</p>
+    </div>
+  );
+}
+
+export default Card;
