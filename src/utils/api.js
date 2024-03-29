453d828d70ea16ecbd7b891af4505651064695e2
bk-git-hub <bkprivate99@gmail.com>
2024-03-29 11:55:49 +0900

    refactor: Use constants for URLs and errorm essages

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,11 +1,16 @@
 const BASE_URL = 'https://bootcamp-api.codeit.kr/api/sample';
 
+const FOLDER_PATH = '/folder';
+const USER_PATH = '/user';
+const FOLDER_LOADING_ERROR_MESSAGE = '폴더 정보를 불러오는데 실패했습니다';
+const USER_LOADING_ERROR_MESSAGE = '이용자 정보를 불러오는데 실패했습니다';
+
 export async function getFolderInfo() {
-  const query = `${BASE_URL}/folder`;
+  const query = `${BASE_URL}${FOLDER_PATH}`;
   const response = await fetch(query);
 
   if (!response.ok) {
-    throw new Error('폴더 정보를 불러오는데 실패했습니다');
+    throw new Error(FOLDER_LOADING_ERROR_MESSAGE);
   }
 
   const result = await response.json();
@@ -13,10 +18,10 @@ export async function getFolderInfo() {
 }
 
 export async function getUserInfo() {
-  const query = `${BASE_URL}/user`;
+  const query = `${BASE_URL}${USER_PATH}`;
   const response = await fetch(query);
   if (!response.ok) {
-    throw new Error('이용자 정보를 불러오는데 실패했습니다');
+    throw new Error(USER_LOADING_ERROR_MESSAGE);
   }
   const result = await response.json();
   return result;
