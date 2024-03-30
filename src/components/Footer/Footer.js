6926ad3338c7b958ade396d69bf4fc60db3cc216
Jisung Jeong <ssjeond@naver.com>
2024-03-30 17:57:46 +0900

    Feat: Add hyperlink to Footer.js sns icons

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -14,13 +14,23 @@ function Footer() {
           <p>FAQ</p>
         </div>
         <div className="footer_icon_div">
-          <img src={facebook} width="20" height="20" alt="페이스북 아이콘" />
-
-          <img src={twitter} width="20" height="20" alt="트위터 아이콘" />
-
-          <img src={youtube} width="20" height="20" alt="유튜브 아이콘" />
-
-          <img src={instagram} width="20" height="20" alt="인스타그램 아이콘" />
+          <a href="https://facebook.com/">
+            <img src={facebook} width="20" height="20" alt="페이스북 아이콘" />
+          </a>
+          <a href="https://twitter.com/">
+            <img src={twitter} width="20" height="20" alt="트위터 아이콘" />
+          </a>
+          <a href="https://youtube.com/">
+            <img src={youtube} width="20" height="20" alt="유튜브 아이콘" />
+          </a>
+          <a href="https://instagram.com/">
+            <img
+              src={instagram}
+              width="20"
+              height="20"
+              alt="인스타그램 아이콘"
+            />
+          </a>
         </div>
       </div>
     </footer>
