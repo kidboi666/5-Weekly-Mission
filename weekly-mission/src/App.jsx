935c0ba8f7061a57b39303d0f8fd119d6b2f7979
hyunseo11 <70734028+hyunseo11@users.noreply.github.com>
2024-03-30 13:21:08 +0900

    feat: 라우터로 링크 공유 페이지 바꾸기

--- weekly-mission/src/App.jsx
+++ weekly-mission/src/App.jsx
@@ -1,30 +1,14 @@
-import { createGlobalStyle } from "styled-components";
-import Nav from "./components/Nav";
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Footer from "./components/Footer";
-
-const GlobalStyle = createGlobalStyle`
-* {
-  box-sizing: border-box;
-  font-family: 'Pretendard', sans-serif;
-  margin: 0;
-}
-
-button {
-  border: none;
-  border-radius: 8px;
-}
-`;
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LinkShare from "./pages/LinkSharePage";
 
 function App() {
   return (
     <>
-      <GlobalStyle />
-      <Nav />
-      <Header />
-      <Main />
-      <Footer />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LinkShare />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
