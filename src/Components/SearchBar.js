aa259ed1e8cc33da4bfe9878c1d1efd2878e1631
전유민 <beforeyum@jeon-yumin-ui-noteubug.local>
2024-03-31 15:16:24 +0900

    검색영역 추가

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,8 +1,13 @@
+import search from '../static/images/search.svg';
+
 function SearchBar() {
 
     return (
         <section className="search">
-            검색
+            <div className="search-bar">
+                <img src={search} alt="돋보기" />
+                <input type="text" className="input-search" placeholder="링크를 검색해보세요."/>
+            </div>
             
         </section>
     );
