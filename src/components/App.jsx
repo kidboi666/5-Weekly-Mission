6ff9539daa20316cb2aad4399cc1de12520cd785
bk-git-hub <bkprivate99@gmail.com>
2024-04-01 13:05:31 +0900

    chore: Move App.jsx to components directory

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import Folder from './Folder/Folder';
+import Footer from './Footer/Footer';
+import Header from './Header/Header';
+import '../assets/css/reset.css';
+export default function App() {
+  return (
+    <>
+      <Header />
+      <Folder />
+      <Footer />
+    </>
+  );
+}
