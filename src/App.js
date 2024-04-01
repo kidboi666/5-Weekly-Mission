f766248992b895444c2e98b3ada6d9afbb53bd9b
jangmoonwon <muneon96@naver.com>
2024-04-01 12:29:59 +0900

    refactor: 수정

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
+import Header from "./components/Header";
+import User from "./components/User";
+import FolderListPage from "./pages/FolderListPage";
+import Footer from "./components/Footer";
+import "./App.css";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <Header />
+      <User />
+      <FolderListPage />
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
