fde8627adbb8800e37be2937963c2d6888b774f9
Kimhansaem <khs_38@naver.com>
2024-04-03 23:38:21 +0900

    feat: 기존 작업 페이지 분리

---
+++ src/pages/Shared.jsx
@@ -0,0 +1,32 @@
+import ConHeader from "../components/ConHeader";
+import Search from "../components/Search";
+import CardList from "../components/CardList";
+import { useEffect, useState } from "react";
+import { fetchData } from "../fetchUtils";
+
+function Shared() {
+    const [folderData, setFolderData] = useState([]);
+    const [cardData, setCardData] = useState([]);
+
+    useEffect(() => {
+        async function fetchDataAndSetState() {
+            const { cardData, folderData } = await fetchData();
+
+            setFolderData(folderData);
+            setCardData(cardData);
+        }
+        fetchDataAndSetState();
+    }, []);
+
+    return (
+        <div className="content-wrap">
+            <ConHeader folderData={folderData} />
+            <div className="wrap">
+                <Search />
+                <CardList cardData={cardData} />
+            </div>
+        </div>
+    );
+}
+
+export default Shared;
