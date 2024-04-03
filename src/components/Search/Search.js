838725a4460ca79cc0ce9d2e09724e05646be54b
daeun <qzsz797979@gmail.com>
2024-04-03 13:30:41 +0900

    Search 컴포넌트 Styled 적용

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,6 +1,6 @@
 import React from "react";
-import "./Search.css";
+import * as S from "./Search.styled";
 
 export default function Search() {
-  return <input className='search-input' placeholder='링크를 검색해 보세요.' />;
+  return <S.StyledInput placeholder='링크를 검색해 보세요.' />;
 }
