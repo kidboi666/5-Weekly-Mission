94c47f5de78c8a9016f1f8c8b67b195bf5e9d7e1
Jisung Jeong <ssjeond@naver.com>
2024-04-03 13:49:12 +0900

    feat: create addLink input

--- src/components/Folderdata/index.jsx
+++ src/components/Folderdata/index.jsx
@@ -1,14 +1,27 @@
 import { useFetch } from "../../utils/hooks/useFetch";
 import styles from "./index.module.css";
+import addLinkIcon from "../../assets/link.svg";
+import linkstyles from "./addlink.module.css";
+import styled from "styled-components";
 
 function FolderData() {
   const folderData = useFetch(
-    "https://bootcamp-api.codeit.kr/api/sample/folder"
+    "https://bootcamp-api.codeit.kr/api/sample/folderrr"
   );
 
+  const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 100%;
+    height: 24.4rem;
+    padding: 2rem 0 6rem;
+    background-color: var(--light-blue);
+  `;
+
   return (
-    <div className={styles.folderInfoContainer}>
-      {folderData && (
+    <Container>
+      {folderData ? (
         <div className={styles.folderInfo}>
           <img
             src={folderData.folder.owner.profileImageSource}
@@ -20,8 +33,22 @@ function FolderData() {
           </span>
           <h1 className={styles.folderName}>{folderData.folder.name}</h1>
         </div>
+      ) : (
+        <div className={linkstyles.addLinkBar}>
+          <input
+            type="search"
+            className={linkstyles.addLinkInput}
+            placeholder="링크를 추가해 보세요."
+          />
+          <img
+            src={addLinkIcon}
+            className={linkstyles.addLinkIcon}
+            alt="링크 추가 아이콘"
+          />
+          <button className={linkstyles.addLinkButton}> 추가하기 </button>
+        </div>
       )}
-    </div>
+    </Container>
   );
 }
 
