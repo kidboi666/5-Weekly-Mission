53a1700294d70a93f67da88f13ae67ca94cba293
bk-git-hub <bkprivate99@gmail.com>
2024-04-04 16:03:13 +0900

    feat: Add camelCase converting functions to unify data format

---
+++ src/utils/convertObjectKeysToCamelCase.js
@@ -0,0 +1,19 @@
+export function convertObjectKeysToCamelCase(obj) {
+  if (typeof obj !== 'object' || obj === null) {
+    return obj;
+  }
+  let camelCaseObj = {};
+  Object.keys(obj).forEach((key) => {
+    const camelCaseKey = convertStringToCamelCase(key);
+    console.log(camelCaseKey, key);
+    camelCaseObj[camelCaseKey] = obj[key];
+  });
+  return camelCaseObj;
+}
+
+export function convertStringToCamelCase(string) {
+  const camelCaseString = string.replace(/_([a-z])/g, (match, letter) =>
+    letter.toUpperCase()
+  );
+  return camelCaseString;
+}
