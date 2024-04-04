7a30fb2506ea067b57a5721afaa32ac5b424894b
bk-git-hub <bkprivate99@gmail.com>
2024-04-04 21:33:00 +0900

    refactor: Refactor router using RouterProvider

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,37 @@
-import ReactDOM from 'react-dom/client';
-import Main from './Main';
+import * as React from 'react';
+import { createRoot } from 'react-dom';
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+  Routes,
+} from 'react-router-dom';
 import './index.css';
+import App from './components/App';
+import SharedPage from './pages/SharedPage/SharedPage';
+import FolderPage from './pages/FolderPage/FolderPage';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Main />);
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Navigate to="/shared" replace />,
+  },
+  {
+    path: '/folder',
+    element: <FolderPage />,
+  },
+  {
+    path: '/shared',
+    element: <SharedPage />,
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router}>
+      <App>
+        <Routes />
+      </App>
+    </RouterProvider>
+  </React.StrictMode>
+);
