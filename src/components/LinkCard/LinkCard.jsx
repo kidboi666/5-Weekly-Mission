d2695d1a56462e493aaf36e50e52a010bddabb1c
bk-git-hub <bkprivate99@gmail.com>
2024-04-04 20:22:04 +0900

    feat: Add kebab button and drop down options in LinkCard component

--- src/components/LinkCard/LinkCard.jsx
+++ src/components/LinkCard/LinkCard.jsx
@@ -1,9 +1,10 @@
 import './LinkCard.css';
 import noImagePlaceholder from '../../assets/images/placeholder_image.png';
+import kebab from '../../assets/images/kebab.svg';
 import {
   STYLE_LINK_CARD_CLASS_NAME,
   STYLE_LINK_CARD_INFO_CLASS_NAME,
-  STYLE_LINK_CARD_TITLE_BAR_CLASS_NAME,
+  STYLE_LINK_CARD_TIMESTAMP_BAR_CLASS_NAME,
   STYLE_TIMESTAMP_CLASS_NAME,
   STYLE_LINK_CARD_DESCRIPTION_CLASS_NAME,
   STYLE_LINK_CARD_CREATED_CLASS_NAME,
@@ -12,8 +13,10 @@ import {
 } from './constants.js';
 import getTimeDifference from '../../utils/time-functions/getTimeDifference.js';
 import formatDate from '../../utils/time-functions/formatDate.js';
+import { useState } from 'react';
 
 export default function LinkCard({ linkCardInfo }) {
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
   const thumbnailURL = linkCardInfo.imageSource
     ? linkCardInfo.imageSource
     : noImagePlaceholder;
@@ -21,6 +24,12 @@ export default function LinkCard({ linkCardInfo }) {
   const url = linkCardInfo.url;
   const createdDate = new Date(linkCardInfo.createdAt);
   const timestamp = getTimeDifference(createdDate);
+
+  const handleKebabClick = (event) => {
+    setIsDropdownOpen(!isDropdownOpen);
+    event.preventDefault();
+  };
+
   return (
     <a
       className={STYLE_LINK_CARD_CLASS_NAME}
@@ -36,8 +45,17 @@ export default function LinkCard({ linkCardInfo }) {
         />
       </div>
       <div className={STYLE_LINK_CARD_INFO_CLASS_NAME}>
-        <div className={STYLE_LINK_CARD_TITLE_BAR_CLASS_NAME}>
+        <div className={STYLE_LINK_CARD_TIMESTAMP_BAR_CLASS_NAME}>
           <span className={STYLE_TIMESTAMP_CLASS_NAME}>{timestamp}</span>
+          <button onClick={handleKebabClick}>
+            <img src={kebab} alt="kebab" />
+          </button>
+          {isDropdownOpen && (
+            <ul>
+              <li>삭제하기</li>
+              <li>폴더에 추가</li>
+            </ul>
+          )}
         </div>
         <p className={STYLE_LINK_CARD_DESCRIPTION_CLASS_NAME}>{description}</p>
         <span className={STYLE_LINK_CARD_CREATED_CLASS_NAME}>
