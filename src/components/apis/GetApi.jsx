2e5029928df0da59f01b150065badbcfff92f50d
yup299 <yup299@gmail.com>
2024-04-05 21:44:51 +0900

    refactor : 코드 간결화

--- src/components/apis/GetApi.jsx
+++ src/components/apis/GetApi.jsx
@@ -4,30 +4,16 @@ const BASE_URL = 'https://bootcamp-api.codeit.kr/';
 
 async function fetchData(url, method = 'GET', body = null) {
   try {
-    let response;
-    switch (method) {
-      case 'GET':
-        response = await axios.get(`${BASE_URL}${url}`);
-        break;
-      case 'POST':
-        response = await axios.post(`${BASE_URL}${url}`, body);
-        break;
-      case 'PUT':
-        response = await axios.put(`${BASE_URL}${url}`, body);
-        break;
-      case 'DELETE':
-        response = await axios.delete(`${BASE_URL}${url}`);
-        break;
-      case 'PATCH':
-        response = await axios.patch(`${BASE_URL}${url}`, body);
-        break;
-      default:
-        throw new Error(`Unsupported method: ${method}`);
-    }
+    const response = await axios({
+      url: `${BASE_URL}${url}`,
+      method,
+      data: body,
+    });
     return { data: response.data, error: null };
   } catch (error) {
     console.error(error);
-    return { data: null, error: error.message };
+    const errorMessage = error.response ? error.response.data : error.message;
+    return { data: null, error: errorMessage };
   }
 }
 
--- src/components/hooks/useFetch.jsx
+++ src/components/hooks/useFetch.jsx
@@ -3,11 +3,12 @@ import fetchData from '../apis/GetApi';
 
 function useFetch(url, method) {
   const [data, setData] = useState(null);
-  const [isLoading, setIsLoading] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchUrl = async () => {
+      setIsLoading(true);
       const result = await fetchData(url, method);
       setData(result.data);
       setIsLoading(false);
