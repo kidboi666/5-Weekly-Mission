197171420dc2b39b8b3da3fae1097d06f6aa934e
hakyoung12 <gkr097@naver.com>
2024-04-05 19:38:02 +0900

    refactor: PostDateUtility 컴포넌트 리팩토링

--- src/components/PostDateUtility.js
+++ src/components/PostDateUtility.js
@@ -17,19 +17,16 @@ export function calculatePostTimeElapsed(value) {
   } else if (timeDifference < HOURS) {
     return `${Math.floor(timeDifference)} minutes ago`;
   } else if (timeDifference < DAYS) {
-    return `${Math.floor(timeDifference)} ${
-      Math.floor(timeDifference) === 1 ? "hour" : "hours"
-    } ago`;
+    const hours = Math.floor(timeDifference / HOURS);
+    return `${hours} ${hours === 1 ? "hour" : "hours"} ago`;
   } else if (timeDifference < MONTH) {
-    return `${Math.floor(timeDifference)} ${
-      Math.floor(timeDifference) === 1 ? "day" : "days"
-    } ago`;
-  } else if (timeDifference < 60 * 24 * 30 * 12) {
-    return `${Math.floor(timeDifference)} ${
-      Math.floor(timeDifference) === 1 ? "month" : "months"
-    } ago`;
+    const days = Math.floor(timeDifference / DAYS);
+    return `${days} ${days === 1 ? "day" : "days"} ago`;
+  } else if (timeDifference < YEARS) {
+    const months = Math.floor(timeDifference / MONTH);
+    return `${months} ${months === 1 ? "month" : "months"} ago`;
+  } else {
+    const years = Math.floor(timeDifference / YEARS);
+    return `${years} ${years === 1 ? "year" : "years"} ago`;
   }
-  return `${Math.floor(timeDifference)} ${
-    Math.floor(timeDifference) === 1 ? "month" : "months"
-  } ago`;
 }
