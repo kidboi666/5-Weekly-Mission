9ea73340fe3f86ecfd0e586e2a75f45ea29c7bc9
yup299 <yup299@gmail.com>
2024-04-05 21:48:10 +0900

    fix : 버그수정

--- src/components/hooks/useFetch.jsx
+++ src/components/hooks/useFetch.jsx
@@ -3,12 +3,11 @@ import fetchData from '../apis/GetApi';
 
 function useFetch(url, method) {
   const [data, setData] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
   const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchUrl = async () => {
-      setIsLoading(true);
       const result = await fetchData(url, method);
       setData(result.data);
       setIsLoading(false);
