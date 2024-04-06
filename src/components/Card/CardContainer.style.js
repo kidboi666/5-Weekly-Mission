8f081238513c0dcffadcfd0993cfbe9f4fa28f71
전유민 <beforeyum@jeon-yumin-ui-noteubug.local>
2024-04-06 17:57:44 +0900

    design: Card Content height 값 변경

--- src/components/Card/CardContainer.style.js
+++ src/components/Card/CardContainer.style.js
@@ -18,6 +18,7 @@ export const CardImage = styled.img`
 
 /* Card 내용 */
 export const CardContent = styled.div`
+  height: 13.5rem;
   padding: 1.5rem 2rem;
   display: flex;
   flex-direction: column;
@@ -52,8 +53,8 @@ export const CardContentCreatedAt = styled.span`
 export const CardWrap = styled.div`
   display: flex;
   flex-direction: column;
-  width: 34rem;
-  height: 33.5rem;
+  width: 32.5rem;
+  height: 32.7rem;
   box-shadow: 0 0.5rem 2.5rem 0 rgba(0, 0, 0, 0.08);
   border-radius: 1.5rem;
   overflow: hidden;
@@ -61,7 +62,7 @@ export const CardWrap = styled.div`
 
   @media (min-width: 768px) {
     width: 34rem;
-    height: 33.4rem;
+    height: 33.5rem;
   }
 
   &:hover ${CardImage} {
