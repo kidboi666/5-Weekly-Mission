d81c9f00db1077dad2404101c3e37aac3cd0296b
SeungRyeolBaek <bsr20030507@g.skku.edu>
2024-04-07 10:47:36 +0900

    feat: NoLink 추가(링크 없을 시 보여짐)

--- src/Folder_and_Shared/feature/CardList/architecture.jsx
+++ src/Folder_and_Shared/feature/CardList/architecture.jsx
@@ -1,3 +1,4 @@
 import { useGetFolders } from "../../data-access/useGetFolders"
 import { AddLinkModal } from "../AddLinkModal"
 import { EditableCard } from "../EditableCard"
+import { NoLink } from "Folder_and_Shared/ui/NoLink"
