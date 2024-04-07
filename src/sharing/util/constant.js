a5fe046298b0d861309e5ef5d297d821eacd1244
LeeSoonGu <rntnsl9280@naver.com>
2024-04-07 17:57:26 +0900

    8주차 위클리 미션

---
+++ src/sharing/util/constant.js
@@ -0,0 +1,24 @@
+export const ROUTE = {
+  landing: "/",
+  login: "/signin",
+  privacy: "/privacy",
+  FAQ: "/faq",
+};
+
+const second = 1000;
+const minute = second * 60;
+const hour = minute * 60;
+const day = hour * 24;
+const month = day * 31;
+const year = month * 12;
+
+export const TIME_IN_MILLISECONDS = {
+  second,
+  minute,
+  hour,
+  day,
+  month,
+  year,
+};
+
+export const ROOT_ID = "root";
---
+++ src/sharing/util/index.js
@@ -0,0 +1,5 @@
+export * from "../../sharing/util/axiosInstance";
+export * from "../../sharing/util/constant";
+export * from "../../sharing/util/getElapsedTime";
+export * from "../../sharing/util/useAsync";
+export * from "../../sharing/util/useEffectOnce";
