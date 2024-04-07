d2e16ae218af42ac18b7431810572e4892a063ba
Kimhansaem <khs_38@naver.com>
2024-04-07 17:59:24 +0900

    feat: cardlist 컴포넌트 추가

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -1,15 +1,28 @@
 import "./CardList.css";
 import Card from "../Card/Card";
 
-function CardList({ cardListData }) {
+function CardList({ cardListData, userFolderDataList }) {
     return (
-        <ul className="card-list">
-            {cardListData.map(({ createdAt, description, id, title, url, imageSource }) => {
-                const card = { createdAt, description, id, title, url, imageSource };
-
-                return <Card card={card} key={id} />;
-            })}
-        </ul>
+        <>
+            {cardListData ? (
+                <ul className="card-list">
+                    {cardListData?.map((card) => {
+                        return <Card card={card} key={card.id} />;
+                    })}
+                </ul>
+            ) : null}
+            {userFolderDataList ? (
+                userFolderDataList?.data?.length ? (
+                    <ul className="card-list">
+                        {userFolderDataList?.data?.map((card) => {
+                            return <Card card={card} key={card.id} />;
+                        })}
+                    </ul>
+                ) : (
+                    <div className="empty-link">저장된 링크가 없습니다.</div>
+                )
+            ) : null}
+        </>
     );
 }
 
