2662795d962e0ffa0684e39063927f8b597ffd0c
Kimhansaem <khs_38@naver.com>
2024-04-07 17:57:37 +0900

    feat: folder 사이트 card 컴포넌트 관련 추가

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,23 @@
-import catImg from "../../assets/images/cat.jpg";
 import "./Card.css";
+import catImg from "../../assets/images/cat.jpg";
 import GetDate from "../../utils/GetDate";
 import GetDaysAgo from "../../utils/GetDaysAgo";
+import useToggle from "../../hooks/useToggle";
+import KebobList from "./KebobList";
+
+import EditableStarButton from "./EditableStarButton";
+import KebobButton from "./KebobButton";
 
 function Card({ card }) {
-    const { createdAt, description, id, title, url, imageSource } = card;
+    const { description, id, title, url } = card;
+    const imageSource = card.image_source ?? card.imageSource;
+    const createdAt = card.created_at ?? card.createdAt;
+    const [display, setDisplay] = useToggle();
+
+    const onClickKebobButton = (e) => {
+        e.preventDefault();
+        setDisplay();
+    };
 
     return (
         <li key={id}>
@@ -15,10 +28,19 @@ function Card({ card }) {
                     ) : (
                         <img src={catImg} alt="이미지가 없습니다." />
                     )}
+                    {card.created_at ? <EditableStarButton /> : null}
                 </div>
                 <div className="text-box">
-                    <div className="time">
-                        <GetDaysAgo createdAt={createdAt} />
+                    <div className="text-menu-wrap">
+                        <div className="time">
+                            <GetDaysAgo createdAt={createdAt} />
+                        </div>
+                        {card.created_at ? (
+                            <>
+                                <KebobButton onClickKebobButton={onClickKebobButton} />
+                                {display === true ? <KebobList /> : null}
+                            </>
+                        ) : null}
                     </div>
                     <p>{description}</p>
                     <div className="date">
