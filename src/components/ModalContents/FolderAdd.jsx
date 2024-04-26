f5c62d4f192bd6a421c029bb501f6976fa0a3dc1
bk-git-hub <bkprivate99@gmail.com>
2024-04-26 15:00:55 +0900

    feat: Add folderAdd component

---
+++ src/components/ModalContents/FolderAdd.jsx
@@ -0,0 +1,11 @@
+import Button from '../Button/Button';
+
+export default function FolderAdd(params) {
+  return (
+    <div>
+      <span>폴더 추가</span>
+      <input />
+      <Button>추가하기</Button>
+    </div>
+  );
+}
