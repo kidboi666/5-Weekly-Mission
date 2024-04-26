334d82854fc2f2547fa37fe851c2781e0727f415
MEGUMMY1 <cys10902@nate.com>
2024-04-27 01:43:25 +0900

    Feat: SNS 공유(링크 복사)

--- src/Components/FolderMain/FolderMain.jsx
+++ src/Components/FolderMain/FolderMain.jsx
@@ -1,19 +1,19 @@
-import React, { useState, useEffect } from 'react';
-import Cards from '../Cards/Cards';
-import Article from '../Article/Article';
-import search_icon from '../../assets/Search.svg';
-import share from '../../assets/share.svg';
-import pen from '../../assets/pen.svg';
-import delete_icon from '../../assets/deleteIcon.svg';
-import styles from './FolderMain.module.css';
-import { FolderDataAll, FolderData } from '../../api/parseData';
-import { useFetch } from '../../hooks/useFetch';
-import { BASE_URL } from '../../constants/baseURL';
-import Modal from '../Modal/Modal';
+import React, { useState, useEffect } from "react";
+import Cards from "../Cards/Cards";
+import Article from "../Article/Article";
+import search_icon from "../../assets/Search.svg";
+import share from "../../assets/share.svg";
+import pen from "../../assets/pen.svg";
+import delete_icon from "../../assets/deleteIcon.svg";
+import styles from "./FolderMain.module.css";
+import { FolderDataAll, FolderData } from "../../api/parseData";
+import { useFetch } from "../../hooks/useFetch";
+import { BASE_URL } from "../../constants/baseURL";
+import Modal from "../Modal/Modal";
 
 function FolderMain() {
-    const [activeButton, setActiveButton] = useState('전체');
-    const [activeButtonId, setActiveButtonId] = useState('');
+    const [activeButton, setActiveButton] = useState("전체");
+    const [activeButtonId, setActiveButtonId] = useState("");
     const folderList = useFetch(`${BASE_URL}users/1/folders`); // 개별 폴더
 
     const handleFolderClick = (folderId, folderName) => {
@@ -38,9 +38,9 @@ function FolderMain() {
     // 모달 오픈 시 스크롤 막기
     useEffect(() => {
         if (modalType) {
-            document.body.style.overflow = 'hidden';
+            document.body.style.overflow = "hidden";
         } else {
-            document.body.style.overflow = 'auto';
+            document.body.style.overflow = "auto";
         }
     }, [modalType]);
 
@@ -49,18 +49,18 @@ function FolderMain() {
             <Article />
             <section className={styles.section}>
                 <div className={styles.search_div}>
-                    <img src={search_icon} width="15" height="15" alt="search_icon" />
-                    <input className={styles.search_input} placeholder="링크를 검색해보세요" />
+                    <img src={search_icon} width='15' height='15' alt='search_icon' />
+                    <input className={styles.search_input} placeholder='링크를 검색해보세요' />
                 </div>
                 <div className={styles.main_btn_div}>
                     <ul className={styles.folder_name_list}>
                         <li>
                             <button
                                 className={`${styles.folder_btn_lg} ${
-                                    activeButton === '전체' && styles.active
+                                    activeButton === "전체" && styles.active
                                 }`}
-                                onClick={() => handleFolderClick('', '전체')}
-                                id="all"
+                                onClick={() => handleFolderClick("", "전체")}
+                                id='all'
                             >
                                 전체
                             </button>
@@ -82,18 +82,18 @@ function FolderMain() {
                     </ul>
                     <button
                         className={styles.add_folder_btn}
-                        id="addFolderBtn"
-                        onClick={() => openModal('addFolder')}
+                        id='addFolderBtn'
+                        onClick={() => openModal("addFolder")}
                     >
                         폴더 추가+
                     </button>
-                    {modalType === 'addFolder' && (
+                    {modalType === "addFolder" && (
                         <Modal
-                            title="폴더 추가"
+                            title='폴더 추가'
                             input
-                            placeholder="내용 입력"
-                            btnText="추가하기"
-                            btnColor="submit"
+                            placeholder='내용 입력'
+                            btnText='추가하기'
+                            btnColor='submit'
                             onClose={closeModal}
                             onSubmit={handleSubmit}
                         />
@@ -101,54 +101,55 @@ function FolderMain() {
                 </div>
                 <div className={styles.main_sub_nav}>
                     <p>{activeButton}</p>
-                    {activeButton !== '전체' && (
+                    {activeButton !== "전체" && (
                         <div className={styles.main_sub_nav_side}>
                             <button
                                 className={styles.main_sub_nav_side_items}
-                                id="shareFolderBtn"
-                                onClick={() => openModal('share')}
+                                id='shareFolderBtn'
+                                onClick={() => openModal("share")}
                             >
-                                <img src={share} width={18} height={18} alt="폴더 공유" />
+                                <img src={share} width={18} height={18} alt='폴더 공유' />
                                 공유
                             </button>
-                            {modalType === 'share' && (
+                            {modalType === "share" && (
                                 <Modal
-                                    title="폴더 공유"
+                                    title='폴더 공유'
                                     subtitle={activeButton}
                                     onClose={closeModal}
+                                    share
                                 />
                             )}
                             <button
                                 className={styles.main_sub_nav_side_items}
-                                onClick={() => openModal('edit')}
+                                onClick={() => openModal("edit")}
                             >
-                                <img src={pen} width={18} height={18} alt="폴더 이름 변경" />
+                                <img src={pen} width={18} height={18} alt='폴더 이름 변경' />
                                 이름 변경
                             </button>
-                            {modalType === 'edit' && (
+                            {modalType === "edit" && (
                                 <Modal
-                                    title="폴더 이름 변경"
+                                    title='폴더 이름 변경'
                                     input
                                     placeholder={activeButton}
-                                    btnColor="submit"
-                                    btnText="변경하기"
+                                    btnColor='submit'
+                                    btnText='변경하기'
                                     onClose={closeModal}
                                     onSubmit={handleSubmit}
                                 />
                             )}
                             <button
                                 className={styles.main_sub_nav_side_items}
-                                onClick={() => openModal('deleteFolder')}
+                                onClick={() => openModal("deleteFolder")}
                             >
-                                <img src={delete_icon} width={18} height={18} alt="폴더 삭제" />
+                                <img src={delete_icon} width={18} height={18} alt='폴더 삭제' />
                                 삭제
                             </button>
-                            {modalType === 'deleteFolder' && (
+                            {modalType === "deleteFolder" && (
                                 <Modal
-                                    title="폴더 삭제"
+                                    title='폴더 삭제'
                                     subtitle={activeButton}
-                                    btnColor="delete"
-                                    btnText="삭제하기"
+                                    btnColor='delete'
+                                    btnText='삭제하기'
                                     onClose={closeModal}
                                     onSubmit={handleSubmit}
                                 />
@@ -157,7 +158,7 @@ function FolderMain() {
                     )}
                 </div>
                 <Cards
-                    items={activeButtonId === '' ? FolderDataAll() : FolderData(activeButtonId)}
+                    items={activeButtonId === "" ? FolderDataAll() : FolderData(activeButtonId)}
                 />
             </section>
         </main>
--- src/Components/Modal/Modal.jsx
+++ src/Components/Modal/Modal.jsx
@@ -1,10 +1,15 @@
 // Modal.jsx
-import React, { useState } from 'react';
-import styles from './Modal.module.css';
-import { useFetch } from '../../hooks/useFetch';
-import { BASE_URL } from '../../constants/baseURL';
-import close from '../../assets/close.svg';
-import check from '../../assets/check.png';
+import React, { useState } from "react";
+import styles from "./Modal.module.css";
+import { useFetch } from "../../hooks/useFetch";
+import { BASE_URL } from "../../constants/baseURL";
+import close from "../../assets/close.svg";
+import check from "../../assets/check.png";
+import kakaotalk from "../../assets/kakaotalk.png";
+import facebook from "../../assets/facebook.png";
+import link from "../../assets/link.png";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function Modal({
     title,
@@ -18,23 +23,35 @@ function Modal({
     onClose,
     onSubmit,
 }) {
-    const [inputValue, setInputValue] = useState('');
+    const [inputValue, setInputValue] = useState("");
     const folderList = useFetch(`${BASE_URL}users/1/folders`);
     const [isError, setIsError] = useState(false);
+    const URL = window.location.href;
 
     const handleChange = (e) => {
         setInputValue(e.target.value);
     };
 
     const handleBlur = () => {
-        if (inputValue.trim() !== '') {
+        if (inputValue.trim() !== "") {
             setIsError(false);
         } else setIsError(true);
     };
 
     const handleSubmit = () => {
         onSubmit(inputValue);
-        setInputValue('');
+        setInputValue("");
+    };
+
+    const handleCopyUrl = () => {
+        navigator.clipboard
+            .writeText(URL)
+            .then(() => {
+                toast.success("클립보드에 복사되었습니다!");
+            })
+            .catch((error) => {
+                toast.error("복사를 실패하였습니다!");
+            });
     };
 
     const [selectedFolderIds, setSelectedFolderIds] = useState([]);
@@ -56,8 +73,8 @@ function Modal({
                 <div className={styles.submit_container}>
                     {input && (
                         <input
-                            className={`${styles.input} ${isError ? styles.error : ''}`}
-                            type="text"
+                            className={`${styles.input} ${isError ? styles.error : ""}`}
+                            type='text'
                             placeholder={placeholder}
                             value={inputValue}
                             onChange={handleChange}
@@ -75,7 +92,7 @@ function Modal({
                                         }`}
                                         onClick={() => handleFolderClick(folder.id)}
                                         onKeyDown={(e) => {
-                                            if (e.key === 'Enter' || e.key === ' ') {
+                                            if (e.key === "Enter" || e.key === " ") {
                                                 handleFolderClick(folder.id);
                                             }
                                         }}
@@ -91,7 +108,7 @@ function Modal({
                                                 src={check}
                                                 width={14}
                                                 height={14}
-                                                alt="check"
+                                                alt='check'
                                                 className={styles.check_icon}
                                             />
                                         )}
@@ -99,12 +116,12 @@ function Modal({
                                 ))}
                         </ul>
                     )}
-                    {btnColor === 'submit' && (
+                    {btnColor === "submit" && (
                         <button
-                            className={`${styles.submit_button} ${isError ? styles.disabled : ''}`}
+                            className={`${styles.submit_button} ${isError ? styles.disabled : ""}`}
                             onClick={handleSubmit}
                             onKeyDown={(e) => {
-                                if (e.key === 'Enter' || e.key === ' ') {
+                                if (e.key === "Enter" || e.key === " ") {
                                     handleSubmit();
                                 }
                             }}
@@ -112,18 +129,56 @@ function Modal({
                             {btnText}
                         </button>
                     )}
-                    {btnColor === 'delete' && (
+                    {btnColor === "delete" && (
                         <button
                             className={styles.delete_button}
                             onClick={handleSubmit}
                             onKeyDown={(e) => {
-                                if (e.key === 'Enter' || e.key === ' ') {
+                                if (e.key === "Enter" || e.key === " ") {
                                     handleSubmit();
                                 }
                             }}
                         >
                             {btnText}
                         </button>
+                    )}
+                    {share && folderList && (
+                        <div className={styles.sns_container}>
+                            <div className={styles.sns}>
+                                <img
+                                    className={styles.sns_image}
+                                    src={kakaotalk}
+                                    width={42}
+                                    height={42}
+                                    alt='kakaotalk'
+                                    tabIndex={0}
+                                />
+                                <span className={styles.sns_text}>카카오톡</span>
+                            </div>
+                            <div className={styles.sns}>
+                                <img
+                                    className={styles.sns_image}
+                                    src={facebook}
+                                    width={42}
+                                    height={42}
+                                    alt='facebook'
+                                    tabIndex={0}
+                                />
+                                <span className={styles.sns_text}>페이스북</span>
+                            </div>
+                            <div className={styles.sns}>
+                                <img
+                                    className={styles.sns_image}
+                                    src={link}
+                                    width={42}
+                                    height={42}
+                                    alt='link'
+                                    tabIndex={0}
+                                    onClick={handleCopyUrl}
+                                />
+                                <span className={styles.sns_text}>링크 복사</span>
+                            </div>
+                        </div>
                     )}
                 </div>
                 <img
@@ -133,14 +188,15 @@ function Modal({
                     className={styles.close_btn}
                     onClick={onClose}
                     onKeyDown={(e) => {
-                        if (e.key === 'Enter' || e.key === ' ') {
+                        if (e.key === "Enter" || e.key === " ") {
                             onClose();
                         }
                     }}
                     tabIndex={0}
-                    alt="close"
+                    alt='close'
                 />
             </div>
+            <ToastContainer position='bottom-center' autoClose={3000} closeOnClick />
         </div>
     );
 }
