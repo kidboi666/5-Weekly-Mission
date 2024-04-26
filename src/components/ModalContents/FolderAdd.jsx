9a62843f7ac8e571bb29e69c98d2e7f6b5aca2d2
bk-git-hub <bkprivate99@gmail.com>
2024-04-26 15:13:05 +0900

    style: Apply style to FolderAdd component

--- src/components/ModalContents/FolderAdd.jsx
+++ src/components/ModalContents/FolderAdd.jsx
@@ -1,11 +1,14 @@
 import Button from '../Button/Button';
+import styles from './ModalContents.module.css';
 
 export default function FolderAdd(params) {
   return (
-    <div>
-      <span>폴더 추가</span>
-      <input />
-      <Button>추가하기</Button>
+    <div className={styles.modalContentWrapper}>
+      <span className={styles.modalHeader}>폴더 추가</span>
+      <form className={styles.modalForm}>
+        <input placeholder='내용 입력' className={styles.modalInput} />
+        <Button className={styles.modalButton}>추가하기</Button>
+      </form>
     </div>
   );
 }
