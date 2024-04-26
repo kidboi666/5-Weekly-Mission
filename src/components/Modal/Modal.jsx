0bc751670e9e65e6cd85b2eed1661d0957cce9b6
bk-git-hub <bkprivate99@gmail.com>
2024-04-26 14:43:15 +0900

    style: Apply style to close button

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,7 +3,7 @@ import styles from './Modal.module.css';
 export default function Modal({ modalText, children, onClose }) {
   return (
     <div className={styles.modalContainer}>
-      <button onClick={onClose}>close</button>
+      <button onClick={onClose} className={styles.closeButton}></button>
       <span className={styles.modalText}>{modalText}</span>
       {children}
     </div>
