77cf100b0ab6f97efa579e35a888a76ba7818db8
Jisung Jeong <ssjeond@naver.com>
2024-04-27 16:33:59 +0900

    fix: Improved collapsing css by modulize

--- src/modal/ModalAddToFolder.jsx
+++ src/modal/ModalAddToFolder.jsx
@@ -1,4 +1,4 @@
-import './modalAddToFolder.css';
+import styles from './modalAddToFolder.module.css';
 import closeIcon from '../assets/_close.svg';
 
 export default function ModalAddToFolder({
@@ -13,29 +13,29 @@ export default function ModalAddToFolder({
 
   return (
     isModalOpen && (
-      <div className="modal">
-        <div className="modal__content">
-          <div className="modal__content__header">{title}</div>
+      <div className={styles.modal}>
+        <div className={styles.modal__content}>
+          <div className={styles.modal__content__header}>{title}</div>
 
-          <div className="modal__content__body">{targetName}</div>
+          <div className={styles.modal__content__body}>{targetName}</div>
 
-          <div className="option__container">
-            <button className="options">
+          <div className={styles.option__container}>
+            <button className={styles.options}>
               <p>코딩팁</p> <span>7개 링크</span>
             </button>
-            <button className="options">
+            <button className={styles.options}>
               <p>채용 사이트</p> <span>12개 링크</span>
             </button>
-            <button className="options">
+            <button className={styles.options}>
               <p>유용한글</p> <span>30개 링크</span>
             </button>
-            <button className="options">
+            <button className={styles.options}>
               <p>나만의 장소</p> <span>3개 링크</span>
             </button>
           </div>
-          <button className="action-button">추가하기</button>
+          <button className={styles.action__button}>추가하기</button>
 
-          <button className="exit_button" onClick={handleCloseModal}>
+          <button className={styles.exit_button} onClick={handleCloseModal}>
             <img src={closeIcon} alt="닫기버튼" />
           </button>
         </div>
