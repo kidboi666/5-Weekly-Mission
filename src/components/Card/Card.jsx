aabd5e3f5f3cb09e74f5dce8af0e27f92f65c9dd
Kimhansaem <khs_38@naver.com>
2024-04-28 12:31:28 +0900

    Refactor: folder일때만 kebob 나오게끔 수정

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -7,10 +7,13 @@ import KebobList from "./KebobList";
 
 import EditableStarButton from "./EditableStarButton";
 import KebobButton from "./KebobButton";
+import { useLocation } from "react-router-dom";
 
 function Card({ card }) {
   const { description, id, title, url, imageSource, createdAt } = card;
   const [display, setDisplay] = useToggle();
+  const location = useLocation();
+
   const onClickKebobButton = (e) => {
     e.preventDefault();
     setDisplay();
@@ -25,14 +28,14 @@ function Card({ card }) {
           ) : (
             <img src={catImg} alt="이미지가 없습니다." />
           )}
-          {card.createdAt ? <EditableStarButton /> : null}
+          {location.pathname === "/folder" ? <EditableStarButton /> : null}
         </div>
         <div className="text-box">
           <div className="text-menu-wrap">
             <div className="time">
               <GetDaysAgo createdAt={createdAt} />
             </div>
-            {card.createdAt ? (
+            {location.pathname === "/folder" ? (
               <>
                 <KebobButton onClickKebobButton={onClickKebobButton} />
                 {display === true ? <KebobList /> : null}
