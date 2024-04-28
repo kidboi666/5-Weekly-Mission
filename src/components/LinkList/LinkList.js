b97257cb1e8fa14149044c395e9e2da45362c1c8
김보민 <bomin@Bomin-MacBook.local>
2024-04-28 16:50:13 +0900

    폴더 구조 수정

---
+++ src/components/LinkList/LinkList.js
@@ -0,0 +1,42 @@
+import "./Linklist.css";
+import { calculatePastTime } from "../../util";
+function LinkItem({ link, createdAt, imageSource }) {
+  console.log(createdAt);
+  const base_image = "images/card-default.png";
+  const url = link.url;
+  const linkImage = imageSource ? imageSource : base_image;
+  return (
+    <a href={url}>
+      <div className="Link-card">
+        <div className="Link-image-wrapper">
+          <img className="Link-image" src={linkImage} alt="링크이미지"></img>
+        </div>
+        <div className="Link-info">
+          <p className="creation-time">{calculatePastTime(createdAt)}</p>
+          <p className="link-description">{link.description}</p>
+          <p>2023. 3. 15</p>
+        </div>
+      </div>
+    </a>
+  );
+}
+
+export function LinkList({ links, createdtime, image }) {
+  return (
+    <div className="Link-container">
+      <ul className="LinkList">
+        {links.map((link) => {
+          return (
+            <li key={link.id}>
+              <LinkItem
+                link={link}
+                createdAt={link[createdtime]}
+                imageSource={link[image]}
+              />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+}
--- src/pages/FolderPage.js
+++ src/pages/FolderPage.js
@@ -1,60 +1,72 @@
-import GlobalStyles from "../components/GlobalStyles"
-import { Header } from "../components/Header";
-import { SearchBar } from "../components/SearchBar";
-import { Footer } from "../components/Footer";
-import { getData } from "../components/api";
-import '../components/SharedPage.css'
-import { LinkInput } from "../components/LinkInput";
-import { Sorting } from "../components/Sorting";
+import GlobalStyles from "../components/GlobalStyles";
+import { Header } from "../components/Header/Header";
+import { SearchBar } from "../components/SearchBar/SearchBar";
+import { Footer } from "../components/Footer/Footer";
+import { getData } from "../util/api";
+import "../components/SharedPage.css";
+import { LinkInput } from "../components/LinkInput/LinkInput";
+import { Sorting } from "../components/Sorting/Sorting";
 import { useEffect, useState } from "react";
-import { ApiUrl } from "../components/url";
-import { LinkList } from "../components/LinkList";
-import { ErrorComponent } from "../components/ErrorComponent";
+import { ApiUrl } from "../util/url";
+import { LinkList } from "../components/LinkList/LinkList";
+import { ErrorComponent } from "../components/ErrorComponent/ErrorComponent";
 
 const user = await getData(ApiUrl.sampleUser);
 const Folders = await getData(ApiUrl.usersFolders);
 const AllLinks = await getData(ApiUrl.usersLinks);
 
 function FolderPage() {
-    const [selectedId, setSelectedId] = useState();
-    const [urlBySelectedId, setUrlBySelectedId ] = useState('');
-    const [links, setLinks] = useState(AllLinks.data);
-    
-    function checkArrayBlank(array) {
-        return array ? array.length === 0 : true;
+  const [selectedId, setSelectedId] = useState();
+  const [urlBySelectedId, setUrlBySelectedId] = useState("");
+  const [links, setLinks] = useState(AllLinks.data);
+
+  function checkArrayBlank(array) {
+    return array ? array.length === 0 : true;
+  }
+
+  useEffect(() => {
+    const newUrl = `${ApiUrl.usersLinks}?folderId=${selectedId}`;
+    setUrlBySelectedId(newUrl);
+  }, [selectedId]);
+
+  useEffect(() => {
+    async function fetchData() {
+      if (selectedId) {
+        const newLinks = await getData(urlBySelectedId);
+        setLinks(newLinks.data);
+      } else setLinks(AllLinks.data);
     }
+    fetchData();
+  }, [urlBySelectedId, selectedId]);
 
-    useEffect(() => {
-        const newUrl = `${ApiUrl.usersLinks}?folderId=${selectedId}`;
-        setUrlBySelectedId(newUrl);
-    }, [selectedId]); // selectedId가 변경될 때만 실행되도록 설정
-
-    useEffect(() => {
-        async function fetchData() {
-            if(selectedId){
-                const newLinks = await getData(urlBySelectedId);
-                setLinks(newLinks.data);
-            }
-            else setLinks(AllLinks.data);
-        }
-        fetchData();    
-    },[urlBySelectedId,selectedId]);
-    
-    return (
+  return (
+    <div>
+      <GlobalStyles />
+      <Header user={user} />
+      <LinkInput />
+      <div className="contents-wrapper">
+        <SearchBar />
         <div>
-            <GlobalStyles />
-            <Header user={user}/>
-            <LinkInput />
-            <div className='contents-wrapper'>
-                <SearchBar />
-                <div >
-                    <Sorting Folders={Folders.data} selectedId={selectedId} setSelectedId={setSelectedId} setUrl={setUrlBySelectedId}/>
-                    {checkArrayBlank(links) ? <ErrorComponent /> : <LinkList links={links} createdtime='created_at' image='image_source' />}
-                </div>
-            </div>
-            <Footer />
+          <Sorting
+            Folders={Folders.data}
+            selectedId={selectedId}
+            setSelectedId={setSelectedId}
+            setUrl={setUrlBySelectedId}
+          />
+          {checkArrayBlank(links) ? (
+            <ErrorComponent />
+          ) : (
+            <LinkList
+              links={links}
+              createdtime="created_at"
+              image="image_source"
+            />
+          )}
         </div>
-    );
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default FolderPage;
