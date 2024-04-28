5c68ddd16d87acc6f6dc19c33bd931834748ebd3
bk-git-hub <bkprivate99@gmail.com>
2024-04-28 19:29:50 +0900

    feat: Add copy url function

--- src/components/ModalContents/ShareModal.jsx
+++ src/components/ModalContents/ShareModal.jsx
@@ -4,18 +4,32 @@ import kakao from '../../assets/images/kakao.svg';
 import styles from './ModalContents.module.css';
 import { facebookShare } from '../../utils/shareFunctions/facebooShare';
 import { kakaoShare } from '../../utils/shareFunctions/kakaoshare';
+import { useState } from 'react';
 
 export default function ShareModal({ headerText, subHeaderText, folderNum }) {
-  const shareUrl = `https://bk-part2.netlify.app/shared/${folderNum}`;
+  const [showToast, setShowToast] = useState(false);
+
+  const SHARE_URL = `https://bk-part2.netlify.app/shared/${folderNum}`;
+
+  const handleCopyLinkClipBoard = async () => {
+    const copiedLink = SHARE_URL;
+    try {
+      await navigator.clipboard.writeText(copiedLink);
+      setShowToast(true);
+      setTimeout(() => setShowToast(false), 3000);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   const SHARE_BUTTONS_INFO = [
-    { text: '카카오톡', icon: kakao, onClick: () => kakaoShare(shareUrl) },
+    { text: '카카오톡', icon: kakao, onClick: () => kakaoShare(SHARE_URL) },
     {
       text: '페이스북',
       icon: facebook,
-      onClick: () => facebookShare(shareUrl),
+      onClick: () => facebookShare(SHARE_URL),
     },
-    { text: '링크 복사', icon: share },
+    { text: '링크 복사', icon: share, onClick: handleCopyLinkClipBoard },
   ];
 
   return (
@@ -36,6 +50,9 @@ export default function ShareModal({ headerText, subHeaderText, folderNum }) {
           </li>
         ))}
       </ul>
+      <div className={`${styles.toast} ${showToast ? styles.show : ''}`}>
+        URL이 복사되었습니다
+      </div>
     </div>
   );
 }
