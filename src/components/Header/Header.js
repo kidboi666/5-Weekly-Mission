7886af2ed906febece69dee237fe58357410d44c
김보민 <bomin@Bomin-MacBook.local>
2024-04-28 16:52:39 +0900

    Refactor(mentor): 삼항 연산자 축약

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,20 +1,24 @@
-import './Header.css';
+import "./Header.css";
 
-export function Header({user}) {
-    return (
-        <header className='header'>
-            <div className='header-contents'>
-                <a href="/">
-                    <img src="images/logo.svg" alt="linklabrary로고" /> 
-                </a>
-                <div className='user-container'>
-                    <div className="icon">
-                        <img src="images/Ellipse21.svg" alt="myicon"></img> 
-                        <img className="profile" src="images/myprofile.png" alt="myprofile"></img>
-                    </div>
-                    <p>{user.email ? user.email : '로그인'}</p>   
-                </div>
-            </div>
-        </header>
-    );
+export function Header({ user }) {
+  return (
+    <header className="header">
+      <div className="header-contents">
+        <a href="/">
+          <img src="images/logo.svg" alt="linklabrary로고" />
+        </a>
+        <div className="user-container">
+          <div className="icon">
+            <img src="images/Ellipse21.svg" alt="myicon"></img>
+            <img
+              className="profile"
+              src="images/myprofile.png"
+              alt="myprofile"
+            ></img>
+          </div>
+          <p>{user.email ?? "로그인"}</p>
+        </div>
+      </div>
+    </header>
+  );
 }
