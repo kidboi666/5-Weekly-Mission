6f47748078a25ce166ac49423c62439a6f912b56
BaeUiJin <baeuijin01@gmail.com>
2024-05-02 12:16:16 +0900

    feat: 임시 랜딩페이지 수정

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,18 +1,17 @@
 import React from "react";
-import { Navigate } from "react-router";
+import { useNavigate } from "react-router-dom";
 
 export const LandingPage = () => {
+  const navigate = useNavigate();
   const goToFolder = () => {
-    Navigate("/folder");
+    navigate("/folder");
   };
-
   const goToShared = () => {
-    Navigate("/shared");
+    navigate("/shared");
   };
 
   return (
     <div>
-      {/* 버튼 추가 */}
       <button onClick={goToFolder}>(클릭) 폴더페이지로 이동</button>
       <br></br>
       <button onClick={goToShared}>(클릭) 링크페이지로 이동</button>
