e3f2b18274bfdaffaf433b28db1c7becc7e17a82
bk-git-hub <bkprivate99@gmail.com>
2024-05-02 13:11:42 +0900

    feat: Add search text delete button and function

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,6 +1,7 @@
 import styles from './SearchBar.module.css'; // Import the CSS module
 import searchIcon from '../../assets/images/search_icon.svg';
 import { useState, ChangeEvent } from 'react';
+import deleteTextIcon from '../../assets/images/delete_text.png';
 
 export const SEARCH_INPUT_ID = 'search-link';
 const SEARCH_INPUT_PLACEHOLDER = '링크를 검색하세요';
@@ -17,6 +18,10 @@ const SearchBar = ({ onSubmit }: SearchBarProps) => {
     setText(e.target.value);
   };
 
+  const handleSearchDelete = () => {
+    setText('');
+  };
+
   return (
     <form onSubmit={onSubmit} className={styles.searchBar}>
       <button className={styles.searchButton} type='submit'>
@@ -31,6 +36,11 @@ const SearchBar = ({ onSubmit }: SearchBarProps) => {
         value={text}
         onChange={handleChange}
       />
+      {text !== '' && (
+        <button onClick={handleSearchDelete} className={styles.searchButton}>
+          <img src={deleteTextIcon} alt='검색어 삭제' />
+        </button>
+      )}
     </form>
   );
 };
