1a1c50b88c62b1e6f388e9ba37da7ef8e9a920f5
daeun <qzsz797979@gmail.com>
2024-05-03 15:39:28 +0900

    refactor(mentor): styled컴포넌트 import하는 방식 * as S 로 통일

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,11 +1,11 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { StyledButton } from "./Button.styled";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import * as S from './Button.styled';
 
-export default function Button({ text, className = "", link }) {
+export default function Button({ text, className = '', link }) {
   return (
-    <StyledButton className={className}>
+    <S.StyledButton className={className}>
       {link ? <Link to={link}>{text}</Link> : <span>{text}</span>}
-    </StyledButton>
+    </S.StyledButton>
   );
 }
