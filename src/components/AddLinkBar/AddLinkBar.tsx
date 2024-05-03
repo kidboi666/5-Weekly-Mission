6604481d9689086016c685ae72bfe64b8c0797ac
bk-git-hub <bkprivate99@gmail.com>
2024-05-03 15:36:20 +0900

    refactor: Declare types for the component's props and ref

--- src/components/AddLinkBar/AddLinkBar.tsx
+++ src/components/AddLinkBar/AddLinkBar.tsx
@@ -1,19 +1,30 @@
+import React, { forwardRef } from 'react';
 import Button from '../Button/Button';
 import linkIcon from '../../assets/images/link.svg';
 import styles from './AddLinkBar.module.css';
-import { forwardRef } from 'react';
 
-const AddLinkBar = forwardRef<any, HTMLDivElement>((props, ref) => {
+interface AddLinkBarProps {
+  isFixed: boolean;
+}
+
+const AddLinkBar = forwardRef<HTMLDivElement, AddLinkBarProps>((props, ref) => {
+  // Conditionally apply the styles based on the value of isFixed prop
+  const containerClassName = props.isFixed
+    ? `${styles.addLinkBarContainer} ${styles.fixed}`
+    : styles.addLinkBarContainer;
+
   return (
-    <div className={styles.addLinkBar} ref={ref}>
-      <div className={styles.inputBox}>
-        <img className={styles.linkIcon} src={linkIcon} alt='링크 추가하기' />
-        <input
-          className={styles.linkInput}
-          placeholder='링크를 추가해 보세요'
-        />
+    <div className={containerClassName}>
+      <div className={styles.addLinkBar} ref={ref}>
+        <div className={styles.inputBox}>
+          <img className={styles.linkIcon} src={linkIcon} alt='링크 추가하기' />
+          <input
+            className={styles.linkInput}
+            placeholder='링크를 추가해 보세요'
+          />
+        </div>
+        <Button>추가하기</Button>
       </div>
-      <Button>추가하기</Button>
     </div>
   );
 });
