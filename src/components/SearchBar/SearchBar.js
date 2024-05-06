272b6b03e63c0160185a07497bd8ceca67980302
김보민 <bomin@Bomin-MacBook.local>
2024-05-06 22:06:20 +0900

    SearchBar에 취소 버튼 만들기

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,24 +1,43 @@
+import { useState } from "react";
 import "./SearchBar.css";
 
 export function SearchBar({ searchKeyWord, setSearchKeyWord }) {
+  const [value, setValue] = useState("");
+
   const handleInputValue = (e) => {
     if (e.key === "Enter") {
       setSearchKeyWord(e.target.value);
     }
   };
 
+  const handleEmptyValue = () => {
+    setSearchKeyWord(null); //값이 바뀌면 search-result-text없애기
+    setValue("");
+  };
+
   return (
-    <div className="search-box">
-      <img src="./images/Search.svg" alt="searcher" />
-      <input
-        placeholder="링크를 검색해보세요"
-        onKeyDown={handleInputValue}
-      ></input>
+    <>
+      <div className="search-box">
+        <div>
+          <img src="./images/Search.svg" alt="searcher" />
+          <input
+            placeholder="링크를 검색해보세요"
+            onKeyDown={handleInputValue}
+            onChange={(e) => setValue(e.target.value)}
+            value={value}
+          />
+        </div>
+        <img
+          src="images/_close.png"
+          alt="닫기 버튼"
+          onClick={handleEmptyValue}
+        ></img>
+      </div>
       {searchKeyWord && (
-        <p>
+        <p className="search-result-text">
           <span>{searchKeyWord}</span>으로 검색한 결과입니다.
         </p>
       )}
-    </div>
+    </>
   );
 }
