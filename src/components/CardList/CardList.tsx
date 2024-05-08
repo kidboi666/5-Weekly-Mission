6f18b661537cf37330aafbd90e8b7cfa961262cc
daeun <qzsz797979@gmail.com>
2024-05-08 10:08:34 +0900

    fix: CardList.tsx 타입에러 수정

--- src/components/CardList/CardList.tsx
+++ src/components/CardList/CardList.tsx
@@ -2,11 +2,23 @@ import React from 'react';
 import * as S from './CardList.styled';
 import Card from '../Card/Card';
 
+interface Props {
+  items: {
+    id: number;
+    createdAt: string;
+    url: string;
+    title: string;
+    imageSource: string;
+  }[];
+  folderNames: string[];
+  itemCountsInEachFolder: number[];
+}
+
 export default function CardList({
   items,
   folderNames,
   itemCountsInEachFolder,
-}) {
+}: Props) {
   return (
     <S.List>
       {items?.map((item) => (
