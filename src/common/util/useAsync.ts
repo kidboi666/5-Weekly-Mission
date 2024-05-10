074009468243d16f22d3111c1c1ac7859ade73c8
BaeUiJin <baeuijin01@gmail.com>
2024-05-10 20:24:48 +0900

    refactor: useAsync.ts, useGetFolder.ts 타입 지정 중

--- src/common/util/useAsync.ts
+++ src/common/util/useAsync.ts
@@ -1,12 +1,18 @@
 import { useState } from "react";
 import { useEffectOnce } from "./useEffectOnce";
+import { DataTypes, FolderResponse } from "@/common/types/data-access-types";
+import { AxiosError, AxiosResponse } from "axios";
 
-export const useAsync = (asyncFunction) => {
-  const [loading, setLoading] = useState(false);
+export const useAsync = (
+  asyncFunction: () => Promise<AxiosResponse<FolderResponse, AxiosError>>
+) => {
+  const [loading, setLoading] = useState<boolean>(false);
+  // !!! 에러 객체의 경우 타입을 어떻게 적어줘야 할까요?
   const [error, setError] = useState<Error | null>(null);
-  const [data, setData] = useState(null);
+  const [data, setData] = useState<DataTypes | null>(null);
 
-  const execute = async () => {
+  // !!! 리스폰스에 따라 try-catch 문에서 반환값이 달라지는데 Promise<T> T 매개변수는 뭐라 지정해줘야 할까요?
+  const execute = async (): Promise<any> => {
     setLoading(true);
     setError(null);
     setData(null);
@@ -14,7 +20,7 @@ export const useAsync = (asyncFunction) => {
       const response = await asyncFunction();
       setData(response?.data);
       return response;
-    } catch (error) {
+    } catch (error: any) {
       setError(error);
     } finally {
       setLoading(false);
--- src/components-FolderPage/data-access-folder/useGetFolder.ts
+++ src/components-FolderPage/data-access-folder/useGetFolder.ts
@@ -1,9 +1,12 @@
 import { mapFolderData } from "@/components-FolderPage/util-map";
 import { useAsync } from "@/common/util";
 import { axiosInstance } from "@/common/util";
+import { AxiosError, AxiosResponse } from "axios";
+import { FolderResponse } from "@/common/types/data-access-types";
 
 export const useGetFolder = () => {
-  const getFolder = () => axiosInstance.get("sample/folder");
+  const getFolder = (): Promise<AxiosResponse<FolderResponse, AxiosError>> =>
+    axiosInstance.get("sample/folder");
   const { loading, error, data } = useAsync(getFolder);
 
   const folderData = mapFolderData(data?.folder);
