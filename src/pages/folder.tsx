5f959fe71c60fa4c249955d7c69dad8851705168
BaeUiJin <baeuijin01@gmail.com>
2024-05-10 14:48:10 +0900

    rename: 폴더, 링크공유 페이지 파일 이름 변경

--- src/pages/folder.tsx
+++ src/pages/folder.tsx
@@ -1,13 +1,13 @@
-import { useGetFolders } from "folder/data-access-folder";
-import { useGetLinks } from "link/data-access-link";
-import { Layout } from "sharing/feature-layout";
-import { FolderLayout } from "page-layout/FolderLayout";
-import { FolderToolBar } from "folder/feature-folder-tool-bar";
-import { SearchBar } from "link/ui-search-bar";
+import { useGetFolders } from "@/folder/data-access-folder";
+import { useGetLinks } from "@/link/data-access-link";
+import { Layout } from "@/sharing/feature-layout";
+import { FolderLayout } from "@/page-layout/FolderLayout";
+import { FolderToolBar } from "@/folder/feature-folder-tool-bar";
+import { SearchBar } from "@/link/ui-search-bar";
 import { useState } from "react";
-import { ALL_LINKS_ID } from "link/data-access-link/constant";
-import { LinkForm } from "link/feature-link-form";
-import { CardList } from "link/feature-card-list";
+import { ALL_LINKS_ID } from "@/link/data-access-link/constant";
+import { LinkForm } from "@/link/feature-link-form";
+import { CardList } from "@/link/feature-card-list";
 
 export const FolderPage = () => {
   const { data: folders } = useGetFolders();
