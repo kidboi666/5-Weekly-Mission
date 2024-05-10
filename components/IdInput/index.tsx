043bc21a489070e09e791e8d05b7d1e44570fa8a
Jisung Jeong <ssjeond@naver.com>
2024-05-11 08:35:10 +0900

    "Feat: Create Input Components for Id and Pwd"

---
+++ components/IdInput/index.tsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import styles from '@/components/IdInput/index.module.css';
+
+function IdInput() {
+  const [value, setValue] = useState<string>('');
+  const [isError, setIsError] = useState(false);
+  const [ErrorMsg, setErrorMsg] = useState('');
+  const [isFocused, setIsFocused] = useState<boolean>(false);
+
+  const handleFocus = () => {
+    setIsFocused(true);
+  };
+
+  const handleBlur = () => {
+    setIsFocused(false);
+    if (value == '') {
+      setIsError(true);
+      setErrorMsg('내용을 입력하세요.');
+    } else {
+      setIsError(false);
+      setErrorMsg('');
+    }
+  };
+
+  return (
+    <>
+      <div className={styles.input__wrapper}>
+        <input
+          type="text"
+          placeholder="내용 입력"
+          alt="아이디 인풋 폼"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          onFocus={handleFocus}
+          onBlur={handleBlur}
+          className={`${styles.input__form} ${
+            isFocused ? styles.focused : ''
+          } ${isError ? styles.error : ''}`}
+        />
+      </div>
+      {isError && <p className={styles.error__msg}>{ErrorMsg}</p>}
+    </>
+  );
+}
+
+export default IdInput;
---
+++ pages/test/index.tsx
@@ -0,0 +1,13 @@
+import IdInput from '@/components/IdInput';
+import PasswordInput from '@/components/PasswordInput';
+
+function Test() {
+  return (
+    <>
+      <PasswordInput />;
+      <IdInput />
+    </>
+  );
+}
+
+export default Test;
