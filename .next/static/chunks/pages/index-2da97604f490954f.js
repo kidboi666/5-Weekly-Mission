e1492dc453acde76ad3895784f36943e6e92fee6
MEGUMMY1 <cys10902@nate.com>
2024-05-13 01:51:43 +0900

    Fix: build error

---
+++ .next/static/chunks/pages/index-2da97604f490954f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[405],{8312:function(n,u,_){(window.__NEXT_P=window.__NEXT_P||[]).push(["/",function(){return _(6209)}])},6209:function(n,u,_){"use strict";_.r(u);var i=_(5893);_(7294),u.default=()=>(0,i.jsx)("div",{id:"root",children:"첫 화면"})}},function(n){n.O(0,[888,774,179],function(){return n(n.s=8312)}),_N_E=n.O()}]);
