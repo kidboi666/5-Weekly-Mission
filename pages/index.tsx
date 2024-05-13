ff3258b531cd1e4ea144adb42c0f9e07bda39de5
MEGUMMY1 <cys10902@nate.com>
2024-05-13 13:04:05 +0900

    Feat: Input components

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import Input from "../src/Components/Input/Input";
 
 const Index = () => {
-    return <div id='root'>첫 화면</div>;
+    return (
+        <div id='root'>
+            첫 화면
+            <Input id='password' placeholder='내용 입력하세요' />
+        </div>
+    );
 };
 
 export default Index;
