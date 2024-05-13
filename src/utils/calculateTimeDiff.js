5959593844df216146a414b1cb4433e95eb4d4b3
eqypo9 <eqypo@naver.com>
2024-05-13 15:01:40 +0900

    feat: week 13 assignment

--- src/utils/calculateTimeDiff.js
+++
@@ -1,9 +0,0 @@
-export function calculateTimeDiff(createdAt) {
-  const currentTime = new Date();
-  const createdTime = new Date(createdAt);
-  const timeDiff = currentTime.getTime() - createdTime.getTime();
-
-  const minutesDiff = Math.round(timeDiff / (1000 * 60));
-
-  return `${minutesDiff} 분 전`;
-}
--- src/utils/formatDate.js
+++
@@ -1,9 +0,0 @@
-export function formatDate(createdAt) {
-  const date = new Date(createdAt);
-
-  const formattedDate = `${date.getFullYear()}-${(date.getMonth() + 1)
-    .toString()
-    .padStart(2, "0")}-${date.getDate().toString().padStart(2, "0")}`;
-
-  return formattedDate;
-}
