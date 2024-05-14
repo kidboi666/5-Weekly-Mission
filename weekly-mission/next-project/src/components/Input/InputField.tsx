1bbf43f120a96fdc115a7b642e895c81370faf6d
hyunseo11 <70734028+hyunseo11@users.noreply.github.com>
2024-05-14 17:59:23 +0900

    fix: input 컴포넌트 타입스크립트로 수정

--- weekly-mission/next-project/src/components/Input/InputField.tsx
+++ weekly-mission/next-project/src/components/Input/InputField.tsx
@@ -1,41 +1,41 @@
-import { useState } from "react";
+import React, { ChangeEvent, useState } from "react";
 import styles from "./InputField.module.css";
 
-const InputField = ({ type, label, placeholder }) => {
-  const [value, setValue] = useState("");
-  const [error, setError] = useState("");
+interface InputFieldProps {
+  type: "email" | "password";
+  label: string;
+  placeholder: string;
+}
 
-  // 이메일 유효성 검사 함수
-  const validateEmail = (value) => {
-    if (!value) {
-      setError("이메일을 입력해주세요.");
-    } else if (!value.includes("@")) {
-      setError("올바른 이메일 형식이 아닙니다.");
-    } else {
-      setError("");
-    }
-  };
+const InputField: React.FC<InputFieldProps> = ({
+  type,
+  label,
+  placeholder,
+}: InputFieldProps) => {
+  const [value, setValue] = useState<string>("");
+  const [error, setError] = useState<string>("");
 
-  // 비밀번호 유효성 검사 함수
-  const validatePassword = (value) => {
-    if (!value) {
-      setError("비밀번호를 입력하세요.");
-    } else {
-      setError("");
-    }
-  };
-
-  const handleChange = (event) => {
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
     setValue(event.target.value);
   };
 
-  // 입력 필드를 벗어날 때 유효성 검사 수행
   const handleBlur = () => {
     if (type === "email") {
-      validateEmail(value);
+      if (!value) {
+        setError("이메일을 입력해주세요.");
+        return;
+      }
+      if (!value.includes("@")) {
+        setError("올바른 이메일 형식이 아닙니다.");
+        return;
+      }
     } else if (type === "password") {
-      validatePassword(value);
+      if (!value) {
+        setError("비밀번호를 입력하세요.");
+        return;
+      }
     }
+    setError("");
   };
 
   // 입력 필드에 포커스가 되었을 때 에러 상태 초기화
