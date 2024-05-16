3a2d6978099de6511073852c63e6a5ebc7debede
kimmiso <vmffjrm@naver.com>
2024-05-16 23:19:33 +0900

    Feat: 로그인 페이지 react-hook-form 적용 및 가입확인 api 연동

--- pages/login/index.tsx
+++ pages/login/index.tsx
@@ -3,9 +3,37 @@ import { JoinBody, JoinAccessControlBox, JoinSocial, JoinTitle, JoinWrap } from 
 import LinkButton from "@/components/common/atoms/LinkButton";
 import Button from "@/components/common/atoms/Button";
 import { ErrorText, FormRowBox, FormWrap } from "@/components/join/formStyle";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { loginForm } from "@/components/join/interfase";
+import { joinInstance } from "@/lib/axios";
+import { useRouter } from "next/router";
 
 export default function Login() {
-  
+  const router = useRouter();
+  const {register, handleSubmit, formState: { errors }, setError} = useForm<loginForm>({ mode:'onBlur' });
+  const [visibility, setVisibility] = useState(false);
+
+  const handleLoginCheck = async (email:loginForm['email'], password:loginForm['password']) => {
+    try {
+      const res = await joinInstance.post('/sign-in',{email, password});
+      const { data } = res;
+      if(data) {
+        localStorage.setItem('linkbrary', data.data.accessToken)
+        router.push('/folder');
+      }
+    } catch {
+      setError('email', { message: '이메일을 확인해 주세요.'})
+      setError('password', { message: '비밀번호를 확인해 주세요.'})
+      return;
+    }
+  }
+
+  const handleValid = (data:loginForm) => {
+    const {email, password} = data;
+    handleLoginCheck(email, password);
+  }
+
   return (
     <JoinWrap className="no-header--container login__wrap">
       <JoinBody>
@@ -21,24 +49,35 @@ export default function Login() {
           </LinkButton>
         </JoinAccessControlBox>
         <FormWrap>
-          <form>
+          <form onSubmit={handleSubmit(handleValid)}>
             <FormRowBox className="input__id">
               <label htmlFor="input__id-element" className="input__id-label">이메일</label>
-              <input type="text" name="email" id="input__id-element" />
-              <ErrorText className="error__text"></ErrorText>
+              <input {...register('email',{
+                 required:'이메일을 입력해 주세요.',
+                 pattern: {
+                   value:/^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/,
+                   message:'올바른 이메일 주소가 아닙니다'
+                 },
+              })} type="email" name="email" id="input__id-element" />
+              <ErrorText className="error__text">{errors.email?.message}</ErrorText>
             </FormRowBox>
             <FormRowBox className="input__password">
               <label htmlFor="input__password-element" className="input__password-label">비밀번호</label>
               <Relative>
-                <input type="password" name="password" id="input__password-element" />
-                <button type="button" className="input__toggle btn__pw">
-                  <img src="/assets/icon/icon-eye-on.svg" alt="비밀번호 보기" className="pw__on" />
-                  {/* <img src="/assets/icon/icon-eye-off.svg" alt="비밀번호 숨기기" className="pw__off" /> */}
-                </button>
+                <input {...register("password",{
+                  required:'비밀번호를 입력해 주세요', 
+                  pattern: {
+                    value:/^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{8,}$/,
+                    message:'비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.'
+                  },
+                })} type={visibility ? 'text' : 'password'} name="password" id="input__password-element" />
+                <Button $btnClass={'button--input-password'} onclick={() => setVisibility((prev) => !prev)}>
+                  <img src={`/assets/icon/icon-eye-${visibility ? 'on' : 'off'}.svg`} alt="비밀번호 보기" />
+                </Button>
               </Relative>
-              <ErrorText className="error__text"></ErrorText>
+              <ErrorText className="error__text">{errors.password?.message}</ErrorText>
             </FormRowBox>
-            <Button $btnClass={`button--gradient large btn_login`}>로그인</Button>
+            <Button $type="submit" $btnClass={`button--gradient large btn_login`}>로그인</Button>
           </form>
         </FormWrap>
         <JoinSocial>
--- pages/signup/index.tsx
+++ pages/signup/index.tsx
@@ -7,51 +7,38 @@ import { useState } from "react";
 import { useForm } from "react-hook-form";
 import { joinInstance } from "@/lib/axios";
 import { useRouter } from "next/router";
+import { IJoinForm } from "@/components/join/interfase";
 
-interface IFormValues {
-  signUpEmail: string;
-  signUpPassword:string;
-  signUpPasswordConfirm:string;
-}
 
-type fomInitial = keyof IFormValues;
 
 export default function SignUp() {
   const router = useRouter();
   const [pwVisibility, setPwVisibility] = useState<Boolean>(false)
   const [pwConfirmVisibility, setPwConfirmVisibility] = useState<Boolean>(false)
-  const {register, handleSubmit, formState: {errors}, setError, setValue} = useForm<IFormValues>({mode:'onBlur'})
+  const {register, handleSubmit, formState: {errors}, setError, setValue} = useForm<IJoinForm>({mode:'onBlur'})
 
-  const handleEmailCheck = async (data:IFormValues['signUpEmail']) => {
-    let response ;
+  const handleEmailCheck = async (email:IJoinForm['email']) => {
     try {
-      response = await joinInstance.post('/check-email', { email: data });
-      if(response?.status === 200 ) {
+      const res = await joinInstance.post('/check-email', { email });
+      if(res) {
         return true;
       } 
     } catch {
-      setError("signUpEmail", { message :  "사용중인 이메일 입니다." },{ shouldFocus:true });
+      setError("email", { message :  "사용중인 이메일 입니다." },{ shouldFocus:true });
       return ;
     }
   }
 
-  const handlePassWordCheck = (password:IFormValues['signUpPassword'], passwordConfirm:IFormValues['signUpPasswordConfirm']) => {
+  const handlePassWordCheck = (password:IJoinForm['password'], passwordConfirm:IJoinForm['passwordConfirm']) => {
     if(password !== passwordConfirm) {
-      setError("signUpPasswordConfirm", { message :  "비밀번호가 다릅니다." },{ shouldFocus:true });
+      setError("passwordConfirm", { message :  "비밀번호가 다릅니다." },{ shouldFocus:true });
     }
   }
 
-  const onValid =  (data:IFormValues) => {
-    const reset = Object.keys(data);
-
+  const handleValid =  (data:IJoinForm) => {
+    const {password, passwordConfirm} = data;
     // 비밀번호 같은지 확인
-    handlePassWordCheck(data.signUpPassword, data.signUpPasswordConfirm)
-
-    // input 초기화
-    for (let key of reset) {
-      setValue(key as fomInitial , '')  ;
-    }
-
+    handlePassWordCheck(password, passwordConfirm)
     router.push('/folder')
   }
   return   (
@@ -69,28 +56,27 @@ export default function SignUp() {
           </LinkButton>
         </JoinAccessControlBox>
         <FormWrap>
-          <form onSubmit={handleSubmit(onValid)}>
+          <form onSubmit={handleSubmit(handleValid)}>
               {/* 이메일 */}
             <FormRowBox className="input__id">
               <label htmlFor="input__id-element" className="input__id-label">이메일</label>
-              <input {...register("signUpEmail", { 
+              <input {...register("email", { 
                 required:'이메일을 입력해 주세요.',
                 pattern: {
                   value:/^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-za-z0-9\-]+/,
                   message:'올바른 이메일 주소가 아닙니다'
                 },
-                validate: (value) => !value.includes('no'),
                 onBlur: (e) => handleEmailCheck(e.target.value),
               })} 
-              type="text" id="input__id-element" placeholder="이메일을 입력해 주세요." />
+              type="email" id="input__id-element" placeholder="이메일을 입력해 주세요." />
               {/* onBlur: (e) => handleEmailCheck(e.target.value) == undefined || '있는 아이디', */}
-              <ErrorText className="error__text">{errors.signUpEmail?.message}</ErrorText>
+              <ErrorText className="error__text">{errors.email?.message}</ErrorText>
             </FormRowBox>
             {/* 비밀번호 */}
             <FormRowBox className="input__password">
               <label htmlFor="input__password-element" className="input__password-label">비밀번호</label>
               <Relative>
-              <input {...register("signUpPassword", { 
+              <input {...register("password", { 
                 required:'비밀번호를 입력해 주세요', 
                 pattern: {
                   value:/^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{8,}$/,
@@ -102,13 +88,13 @@ export default function SignUp() {
                   <img src={`/assets/icon/icon-eye-${pwVisibility ? 'on' : 'off'}.svg`} alt="비밀번호 보기" />
                 </Button>
               </Relative>
-              <ErrorText className="error__text">{errors.signUpPassword?.message}</ErrorText>
+              <ErrorText className="error__text">{errors.password?.message}</ErrorText>
             </FormRowBox>
             {/* 비밀번호 확인*/}
             <FormRowBox className="input__password__confilrm">
               <label htmlFor="input__password-confilrm" className="input__password-label">비밀번호 확인</label>
               <Relative>
-              <input {...register("signUpPasswordConfirm", { 
+              <input {...register("passwordConfirm", { 
                 required:'비밀번호를 다시 한번 입력해 주세요', 
                 pattern: {
                   value:/^(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]{8,}$/,
@@ -120,7 +106,7 @@ export default function SignUp() {
                   <img src={`/assets/icon/icon-eye-${pwConfirmVisibility ? 'on' : 'off'}.svg`} alt="비밀번호 보기" />
                 </Button>
               </Relative>
-              <ErrorText className="error__text">{errors.signUpPasswordConfirm?.message}</ErrorText>
+              <ErrorText className="error__text">{errors.passwordConfirm?.message}</ErrorText>
             </FormRowBox>
             <Button $type="submit" $btnClass={`button--gradient large btn_login`}>회원가입</Button>
           </form>
