2ffc256e14b8a3958d58a9a50100ad0d39d4585a
은지 <103625348+eunji-0623@users.noreply.github.com>
2024-05-17 16:13:43 +0900

    로그인/회원가입 input 컴포넌트 구현

---
+++ weekly/components/Auth/SignInput.tsx
@@ -0,0 +1,25 @@
+import styled from 'styled-components';
+
+type Props = {
+  type: string;
+  placeholder: string;
+};
+
+function SignInput({ type, placeholder }: Props) {
+  return <StyledInput type={type} placeholder={placeholder} />;
+}
+
+const StyledInput = styled.input`
+  width: 400px;
+  padding: 18px 15px;
+  border-radius: 8px;
+  border: 1px solid var(--Linkbrary-gray20, #ccd5e3);
+  background: var(--Linkbrary-white, #fff);
+
+  &:focus {
+    border: 1px solid var(--Linkbrary-primary-color, #6d6afe);
+    outline: none;
+  }
+`;
+
+export default SignInput;
--- weekly/pages/signin.tsx
+++ weekly/pages/signin.tsx
@@ -1,7 +1,13 @@
+import SignInput from '@/components/Auth/SignInput';
 import styled from 'styled-components';
 
 function SignInPage() {
-  return <></>;
+  return (
+    <>
+      <SignInput type="email" placeholder="이메일을 입력해 주세요" />
+      <SignInput type="password" placeholder="비밀번호를 입력해 주세요" />
+    </>
+  );
 }
 
 export default SignInPage;
