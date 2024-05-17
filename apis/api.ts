460eaa7addf02ef7a2f33b364f15064ced153a4e
daeun <qzsz797979@gmail.com>
2024-05-17 17:27:58 +0900

    fix: fetch => axios 수정했을 때 에러 수정

--- apis/api.ts
+++ apis/api.ts
@@ -1,49 +1,44 @@
+import axios from 'axios';
+
 const BASIC_URL = 'https://bootcamp-api.codeit.kr/api';
 
 export async function getUser() {
-  const response = await fetch(`${BASIC_URL}/users/1`);
-  if (!response.ok) {
+  const response = await axios.get(`${BASIC_URL}/users/1`);
+  if (response.status < 200 || response.status >= 300) {
     throw new Error('사용자 데이터를 불러오는데 실패했습니다.');
   }
-  const body = await response.json();
-  const result = body.data[0];
+  const result = response.data.data[0];
   return result;
 }
 
 export async function postSignUp(id: string, pw: string) {
-  const response = await fetch(`${BASIC_URL}/sign-up`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      email: id,
-      password: pw,
-    }),
+  const response = await axios.post(`${BASIC_URL}/sign-up`, {
+    email: id,
+    password: pw,
   });
-  const body = await response.json();
-  const result = body.data;
+  if (response.status < 200 || response.status >= 300) {
+    throw new Error('회원가입에 실패했습니다.');
+  }
+  const result = response.data.data;
   return result;
 }
 
 export async function getFolders(folderId: number) {
   const queryParam = folderId === undefined ? '' : `?folderId=${folderId}`;
-  const response = await fetch(`${BASIC_URL}/users/1/folders${queryParam}`);
-  if (!response.ok) {
+  const response = await axios.get(`${BASIC_URL}/users/1/folders${queryParam}`);
+  if (response.status < 200 || response.status >= 300) {
     throw new Error('사용자 데이터를 불러오는데 실패했습니다.');
   }
-  const body = await response.json();
-  const result = body.data;
+  const result = response.data.data;
   return result;
 }
 
 export async function getLinks(folderId?: number) {
   const queryParam = folderId === 0 ? '' : `?folderId=${folderId}`;
-  const response = await fetch(`${BASIC_URL}/users/1/links${queryParam}`);
-  if (!response.ok) {
+  const response = await axios.get(`${BASIC_URL}/users/1/links${queryParam}`);
+  if (response.status < 200 || response.status >= 300) {
     throw new Error('사용자 데이터를 불러오는데 실패했습니다.');
   }
-  const body = await response.json();
-  const data = body.data;
+  const data = response.data.data;
   return data;
 }
