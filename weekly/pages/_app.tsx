ea7e2f4c46642713d4e43a17e254ff30512b9ca5
은지 <103625348+eunji-0623@users.noreply.github.com>
2024-05-17 16:26:10 +0900

    global 스타일 추가, Pretendard 폰트 적용

--- weekly/pages/_app.tsx
+++ weekly/pages/_app.tsx
@@ -1,5 +1,18 @@
 import type { AppProps } from 'next/app';
+import localFont from 'next/font/local';
+import '@/styles/global.css';
+
+const pretendard = localFont({
+  src: '../static/fonts/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '45 920',
+  variable: '--font-pretendard',
+});
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={pretendard.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
