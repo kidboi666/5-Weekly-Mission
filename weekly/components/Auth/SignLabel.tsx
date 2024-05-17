3e73eede17f0a089a31f6115b5e6f07f4079f5a5
은지 <103625348+eunji-0623@users.noreply.github.com>
2024-05-17 16:31:02 +0900

    로그인/회원가입 Label 컴포넌트 구현

---
+++ weekly/components/Auth/SignLabel.tsx
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const SignLabel = styled.label`
+  color: var(--black, #000);
+  font-size: 14px;
+  padding-bottom: 12px;
+`;
+
+export default SignLabel;
--- weekly/pages/signin.tsx
+++ weekly/pages/signin.tsx
@@ -1,10 +1,13 @@
 import SignInput from '@/components/Auth/SignInput';
+import SignLabel from '@/components/Auth/SignLabel';
 import styled from 'styled-components';
 
 function SignInPage() {
   return (
     <>
+      <SignLabel>이메일</SignLabel>
       <SignInput type="email" placeholder="이메일을 입력해 주세요" />
+      <SignLabel>비밀번호</SignLabel>
       <SignInput type="password" placeholder="비밀번호를 입력해 주세요" />
     </>
   );
