f58cf0a087baf899b38fdd884a47e10e5f0c9a9c
은지 <103625348+eunji-0623@users.noreply.github.com>
2024-05-18 15:29:13 +0900

    이메일 유효성 검사 구현

--- weekly/components/Auth/SignInput.tsx
+++ weekly/components/Auth/SignInput.tsx
@@ -1,19 +1,33 @@
+import { ChangeEvent } from 'react';
 import styled from 'styled-components';
 
 type Props = {
   type: string;
   placeholder: string;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+  onBlur: () => void;
+  isValid: boolean;
+  message: string;
 };
 
-function SignInput({ type, placeholder }: Props) {
-  return <StyledInput type={type} placeholder={placeholder} />;
+function SignInput({ type, placeholder, onChange, onBlur, isValid, message }: Props) {
+  return (
+    <>
+      <StyledInput type={type} placeholder={placeholder} onChange={onChange} onBlur={onBlur} data-isValid={isValid} />
+      {isValid ? <ErrorMessage>{message}</ErrorMessage> : null}
+    </>
+  );
 }
 
-const StyledInput = styled.input`
+interface StyleProps {
+  'data-isValid': boolean;
+}
+
+const StyledInput = styled.input<StyleProps>`
   width: 400px;
   padding: 18px 15px;
   border-radius: 8px;
-  border: 1px solid var(--Linkbrary-gray20, #ccd5e3);
+  border: 1px solid ${(props) => (props['data-isValid'] ? 'var(--Linkbrary-red, #FF5B56)' : 'var(--Linkbrary-gray20, #ccd5e3)')};
   background: var(--Linkbrary-white, #fff);
   margin-top: 12px;
 
@@ -21,6 +35,12 @@ const StyledInput = styled.input`
     border: 1px solid var(--Linkbrary-primary-color, #6d6afe);
     outline: none;
   }
+`;
+
+const ErrorMessage = styled.p`
+  color: var(--Linkbrary-red, #ff5b56);
+  font-size: 14px;
+  margin-top: 6px;
 `;
 
 export default SignInput;
--- weekly/pages/signin.tsx
+++ weekly/pages/signin.tsx
@@ -1,3 +1,5 @@
+import { useState, ChangeEvent } from 'react';
+import isValidEmail from '@/utils/isValidEmail';
 import Logo from '@/components/Auth/Logo';
 import SignInput from '@/components/Auth/SignInput';
 import SignLabel from '@/components/Auth/SignLabel';
@@ -6,6 +8,26 @@ import GradientButton from '@/components/Button/GradientButton';
 import styled from 'styled-components';
 
 function SignInPage() {
+  const [iSInvalid, setIsInvalid] = useState(false);
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const onChangeEmail = (event: ChangeEvent<HTMLInputElement>) => {
+    setEmail(event.target.value);
+  };
+
+  const onBlurEmail = () => {
+    if (email === '') {
+      setIsInvalid(true);
+      setMessage('이메일을 입력해주세요');
+    } else if (!isValidEmail(email)) {
+      setIsInvalid(true);
+      setMessage('올바른 이메일 주소가 아닙니다.');
+    } else {
+      setIsInvalid(false);
+    }
+  };
+
   return (
     <SigninWrapper>
       <SignForm>
@@ -13,11 +35,11 @@ function SignInPage() {
         <div>
           <SignLabel>
             이메일
-            <SignInput type="email" placeholder="이메일을 입력해 주세요" />
+            <SignInput type="email" placeholder="이메일을 입력해 주세요" onChange={onChangeEmail} onBlur={onBlurEmail} isValid={iSInvalid} message={message} />
           </SignLabel>
           <SignLabel>
             비밀번호
-            <SignInput type="password" placeholder="비밀번호를 입력해 주세요" />
+            {/* <SignInput type="password" placeholder="비밀번호를 입력해 주세요" /> */}
           </SignLabel>
         </div>
         <GradientButton size="auto">로그인</GradientButton>
