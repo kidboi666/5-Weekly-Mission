8414f7c75c902f6969e443d521f973ab0c21932b
은지 <103625348+eunji-0623@users.noreply.github.com>
2024-05-18 16:11:05 +0900

    회원가입 페이지 구현

---
+++ weekly/pages/signup.tsx
@@ -0,0 +1,50 @@
+import Logo from '@/components/Auth/Logo';
+import SignInput from '@/components/Auth/SignInput';
+import SignLabel from '@/components/Auth/SignLabel';
+import Social from '@/components/Auth/Social';
+import GradientButton from '@/components/Button/GradientButton';
+import styled from 'styled-components';
+
+function SignInPage() {
+  return (
+    <SigninWrapper>
+      <SignForm>
+        <Logo />
+        <div>
+          <SignLabel>
+            이메일
+            <SignInput type="email" placeholder="이메일을 입력해 주세요" id="email" />
+          </SignLabel>
+          <SignLabel>
+            비밀번호
+            <SignInput type="password" placeholder="비밀번호를 입력해 주세요" id="password" />
+          </SignLabel>
+          <SignLabel>
+            비밀번호 확인
+            <SignInput type="password" placeholder="비밀번호를 한번 더 입력해 주세요" id="passwordCheck" />
+          </SignLabel>
+        </div>
+        <GradientButton size="auto">회원가입</GradientButton>
+        <Social />
+      </SignForm>
+    </SigninWrapper>
+  );
+}
+
+const SignForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 400px;
+  gap: 30px;
+`;
+
+const SigninWrapper = styled.div`
+  display: flex;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  background: var(--Linkbrary-bg, #f0f6ff);
+`;
+
+export default SignInPage;
