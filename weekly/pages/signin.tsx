635dc2d26e4e51cdabf4eb9daa22e47c842353ba
은지 <103625348+eunji-0623@users.noreply.github.com>
2024-05-18 14:21:32 +0900

    소셜 로그인 관련 컴포넌트 구현

--- weekly/pages/signin.tsx
+++ weekly/pages/signin.tsx
@@ -1,23 +1,27 @@
 import SignInput from '@/components/Auth/SignInput';
 import SignLabel from '@/components/Auth/SignLabel';
+import Social from '@/components/Auth/Social';
 import GradientButton from '@/components/Button/GradientButton';
 import styled from 'styled-components';
 
 function SignInPage() {
   return (
-    <SignForm>
-      <div>
-        <SignLabel>
-          이메일
-          <SignInput type="email" placeholder="이메일을 입력해 주세요" />
-        </SignLabel>
-        <SignLabel>
-          비밀번호
-          <SignInput type="password" placeholder="비밀번호를 입력해 주세요" />
-        </SignLabel>
-      </div>
-      <GradientButton size="auto">로그인</GradientButton>
-    </SignForm>
+    <SigninWrapper>
+      <SignForm>
+        <div>
+          <SignLabel>
+            이메일
+            <SignInput type="email" placeholder="이메일을 입력해 주세요" />
+          </SignLabel>
+          <SignLabel>
+            비밀번호
+            <SignInput type="password" placeholder="비밀번호를 입력해 주세요" />
+          </SignLabel>
+        </div>
+        <GradientButton size="auto">로그인</GradientButton>
+        <Social />
+      </SignForm>
+    </SigninWrapper>
   );
 }
 
@@ -25,6 +29,12 @@ const SignForm = styled.form`
   display: flex;
   flex-direction: column;
   width: 400px;
+`;
+
+const SigninWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 export default SignInPage;
